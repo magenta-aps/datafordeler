c43ee943f621c706d1a8bd30319d2a95bc1aa8ff
Vagrant Default User <vagrant@stretch.localdomain>
2017-09-19 12:59:19 +0000

    adding translation files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -71,7 +71,10 @@ release = u'1'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = None
+language = da
+locale_dirs = [
+    'locale/'
+]
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
