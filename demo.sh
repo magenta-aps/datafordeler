aaee8fe0ad70ae352c30fb3f704ba6391c7065d4
Emil Madsen <emil@magenta.dk>
2017-09-19 16:59:30 +0200

    Finalizing, and fixing demo.sh again

--- demo.sh
+++ demo.sh
@@ -1,11 +1,13 @@
 #!/bin/bash
 
-vagrant halt
-PLAYBOOK='demo.yml' vagrant up --provision
-vagrant halt
+cd vagrant/
+vagrant destroy
 PLAYBOOK='demo.yml' vagrant up
-IFCONFIG=$(./vagrant_sudo.sh 'ifconfig eth0')
-IP=$(echo "$IFCONFIG" | grep 'inet addr:' | cut -d: -f2 | awk '{ print $1}')
-
+vagrant halt
+vagrant up
+IFCONFIG=$(./vagrant_sudo.sh 'ip addr show eth0')
+IP=$(echo "$IFCONFIG" | grep -o "inet [0-9]*\.[0-9]*\.[0-9]*\.[0-9]*" | cut -f2 -d' ')
+ 
 echo "Documentation generated, and served at:"
 echo -e "\t http://$IP:8000/"
+cd ..
