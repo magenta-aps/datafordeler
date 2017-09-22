4133a153f7853f2279e57c286d5c3d2b4a439d00
Vagrant Default User <vagrant@stretch.localdomain>
2017-09-22 12:39:03 +0000

    fix language toggle scripts

--- docs/source/dafo_docs_theme/static/js/main.js
+++ docs/source/dafo_docs_theme/static/js/main.js
@@ -1,37 +1,29 @@
 // Highlight selected language in language picker
-console.log(document.querySelector(".lang-da"));
 var currentLangLink,
     daLangLink = document.querySelector(".lang-da"),
     klLangLink = document.querySelector(".lang-kl"),
     enLangLink = document.querySelector(".lang-en"),
     currentLangUrl = location.pathname.slice(0,4);
-console.log('daLangLink');
-console.log(daLangLink);
-console.log('currentLangUrl');
-console.log(currentLangUrl);
+    
 if (currentLangUrl === '/da/') {
-currentLangLink = daLangLink;
-currentLangLink.classList.add('active');
+    currentLangLink = daLangLink;
+    currentLangLink.classList.add('active');
 } else if (currentLangUrl === '/kl/') {
-currentLangLink = klLangLink;
-currentLangLink.classList.add('active');
+    currentLangLink = klLangLink;
+    currentLangLink.classList.add('active');
 } else if (currentLangUrl === '/en/') {
-currentLangLink = enLangLink;
-currentLangLink.classList.add('active');
+    currentLangLink = enLangLink;
+    currentLangLink.classList.add('active');
 }
 
-
 function jumpToLang(ev,lang) {
-    ev.preventDefault(); 
-    console.log('location.pathname');
-    console.log(location.pathname);
+    ev.preventDefault();
     var urlPath = location.pathname.slice(4);
     urlPath = '/' + lang + '/' + urlPath;
-    console.log('Going to ' + location.host + location.urlPath);
-    location.href = location.host + location.urlPath;
+    location.href = location.host + urlPath;
 }
 
 // When clicking language picker, redirect to same page with different translation
 daLangLink.addEventListener("click", function(event){ jumpToLang(event,'da') });
-//klLangLink.addEventListener("click", jumpToLang('kl'));
-//enLangLink.addEventListener("click", jumpToLang('en'));
+klLangLink.addEventListener("click", function(event){ jumpToLang(event,'kl') });
+enLangLink.addEventListener("click", function(event){ jumpToLang(event,'en') });
