eb1b9d7b737cff3b5aab08e289a02b10e2425b8e
Emil Madsen <emil@magenta.dk>
2017-10-03 10:36:20 +0200

    Remove deinit from release.sh

--- release.sh
+++ release.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
-git submodule deinit -f vagrant/
-git submodule deinit -f ansible/roles/vim
-sed -i "s#git@github.com:#https://github.com/#g" .gitmodules
-
 git rm -r vagrant/
 git rm -r ansible/roles/vim
+sed -i "s#git@github.com:#https://github.com/#g" .gitmodules
 
 git submodule update --init
