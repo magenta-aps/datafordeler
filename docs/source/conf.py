81f162af23fd4145d94fc4560be1fa6041b21052
Emil Madsen <emil@magenta.dk>
2017-11-30 16:01:24 +0100

    New po files

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -72,6 +72,8 @@ release = u'1'
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
 language = 'da'
+locale_dirs = ['locales/']
+gettext_compact = False
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
