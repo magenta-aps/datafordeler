de3e5811edf52e0a27068e0f5cbd14c75bad49a2
Efrin González <efringonzalez@gmail.com>
2018-04-05 15:12:14 +0200

    Statistik component

---
+++ plugin/statistik/src/main/java/dk/magenta/datafordeler/datafordeler/statistik/DatafordelerPluginStatistikApplication.java
@@ -0,0 +1,12 @@
+package dk.magenta.datafordeler.datafordeler.statistik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatafordelerPluginStatistikApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatafordelerPluginStatistikApplication.class, args);
+    }
+}
