ba5361337b0f2882138fe78fdc3ef0303fb0897b
Mads Møller Johansen <mmj@magenta.dk>
2020-03-25 17:20:40 +0100

    Adding searchparameters for geo-module

--- plugin/geo/src/test/java/TestGenericServices.java
+++ plugin/geo/src/test/java/TestGenericServices.java
@@ -31,26 +31,38 @@ public class TestGenericServices extends GeoTest {
 
     @Test
     public void testMunicipalityService() throws IOException {
-        ResponseEntity<String> response = this.lookup("/geo/municipality/1/rest/search?fmt=dataonly&Kommunekode=956&navn=Kommuneqarfik Sermersooq");
+        ResponseEntity<String> response = this.lookup("/geo/municipality/1/rest/search?fmt=dataonly&kode=956");
         Assert.assertEquals(200, response.getStatusCode().value());
         ObjectNode responseContent = (ObjectNode) objectMapper.readTree(response.getBody());
-        ObjectNode munipialicity = (ObjectNode) responseContent.get("results").get(0);
-        Assert.assertEquals("33960e68-2f0a-4cb0-bb3d-02d9f0b21304", munipialicity.get("uuid").textValue());
-        Assert.assertEquals("5cc15446cddb4633b6832847b6f5d66d", munipialicity.get("sumiffiik").textValue());
-        Assert.assertEquals(956, munipialicity.get("kommunekode").asInt());
-        Assert.assertNull(munipialicity.get("registreringFra"));
+        ObjectNode munipialicity1 = (ObjectNode) responseContent.get("results").get(0);
+        Assert.assertEquals("33960e68-2f0a-4cb0-bb3d-02d9f0b21304", munipialicity1.get("uuid").textValue());
+        Assert.assertEquals("5cc15446cddb4633b6832847b6f5d66d", munipialicity1.get("sumiffiik").textValue());
+        Assert.assertEquals(956, munipialicity1.get("kommunekode").asInt());
+        Assert.assertNull(munipialicity1.get("registreringFra"));
+
+        response = this.lookup("/geo/municipality/1/rest/search?fmt=dataonly&navn=Kommuneqarfik Sermersooq");
+        Assert.assertEquals(200, response.getStatusCode().value());
+        responseContent = (ObjectNode) objectMapper.readTree(response.getBody());
+        ObjectNode munipialicity2 = (ObjectNode) responseContent.get("results").get(0);
+        Assert.assertEquals(munipialicity1, munipialicity2);
     }
 
 
     @Test
     public void testPostcodeService() throws IOException {
-        ResponseEntity<String> response = this.lookup("/geo/postcode/1/rest/search?fmt=dataonly&kode=3910&navn=Kangerlussuaq");
+        ResponseEntity<String> response = this.lookup("/geo/postcode/1/rest/search?fmt=dataonly&kode=3910");
         Assert.assertEquals(200, response.getStatusCode().value());
         ObjectNode responseContent = (ObjectNode) objectMapper.readTree(response.getBody());
-        ObjectNode munipialicity = (ObjectNode) responseContent.get("results").get(0);
-        Assert.assertEquals("c8d54254-0086-3554-a4ce-424e4ebfd072", munipialicity.get("uuid").textValue());
-        Assert.assertNotNull(munipialicity.get("postdistrikt"));
-        Assert.assertNull(munipialicity.get("registreringFra"));
+        ObjectNode munipialicity1 = (ObjectNode) responseContent.get("results").get(0);
+        Assert.assertEquals("c8d54254-0086-3554-a4ce-424e4ebfd072", munipialicity1.get("uuid").textValue());
+        Assert.assertNotNull(munipialicity1.get("postdistrikt"));
+        Assert.assertNull(munipialicity1.get("registreringFra"));
+
+        response = this.lookup("/geo/postcode/1/rest/search?fmt=dataonly&navn=Kangerlussuaq");
+        Assert.assertEquals(200, response.getStatusCode().value());
+        responseContent = (ObjectNode) objectMapper.readTree(response.getBody());
+        ObjectNode munipialicity2 = (ObjectNode) responseContent.get("results").get(0);
+        Assert.assertEquals(munipialicity1, munipialicity2);
     }
 
     @Test
@@ -58,11 +70,22 @@ public class TestGenericServices extends GeoTest {
         ResponseEntity<String> response = this.lookup("/geo/locality/1/rest/search?fmt=dataonly&kode=0600");
         Assert.assertEquals(200, response.getStatusCode().value());
         ObjectNode responseContent = (ObjectNode) objectMapper.readTree(response.getBody());
-        ObjectNode munipialicity = (ObjectNode) responseContent.get("results").get(0);
-        Assert.assertEquals("f0966470-f09f-474d-a820-e8a46ed6fcc7", munipialicity.get("uuid").textValue());
-        Assert.assertEquals("4F46D110-E6AD-46D1-B403-B12063152564", munipialicity.get("sumiffiik").textValue());
-        Assert.assertEquals("0600", munipialicity.get("lokalitetskode").textValue());
-        Assert.assertNull(munipialicity.get("registreringFra"));
+        ObjectNode munipialicity1 = (ObjectNode) responseContent.get("results").get(0);
+        Assert.assertEquals("f0966470-f09f-474d-a820-e8a46ed6fcc7", munipialicity1.get("uuid").textValue());
+        Assert.assertEquals("4F46D110-E6AD-46D1-B403-B12063152564", munipialicity1.get("sumiffiik").textValue());
+        Assert.assertEquals("0600", munipialicity1.get("lokalitetskode").textValue());
+        Assert.assertNull(munipialicity1.get("registreringFra"));
+
+        response = this.lookup("/geo/locality/1/rest/search?fmt=dataonly&kommune=956");
+        responseContent = (ObjectNode) objectMapper.readTree(response.getBody());
+        ObjectNode munipialicity2 = (ObjectNode) responseContent.get("results").get(0);
+        Assert.assertEquals(munipialicity1, munipialicity2);
+
+
+        /*response = this.lookup("/geo/locality/1/rest/search?fmt=dataonly&navn=Nuuk");
+        responseContent = (ObjectNode) objectMapper.readTree(response.getBody());
+        ObjectNode munipialicity2 = (ObjectNode) responseContent.get("results").get(0);
+        Assert.assertEquals(munipialicity1, munipialicity2);*/
     }
 
     @Test
@@ -70,11 +93,32 @@ public class TestGenericServices extends GeoTest {
         ResponseEntity<String> response = this.lookup("/geo/road/1/rest/search?fmt=dataonly&kode=0254");
         Assert.assertEquals(200, response.getStatusCode().value());
         ObjectNode responseContent = (ObjectNode) objectMapper.readTree(response.getBody());
-        ObjectNode road = (ObjectNode) responseContent.get("results").get(0);
-        Assert.assertEquals("e1274f15-9e2b-4b6e-8b7d-c8078df65aa2", road.get("uuid").textValue());
-        Assert.assertEquals("{961FFC61-8B04-45F3-80FD-509B0676FEF6}", road.get("sumiffiik").textValue());
-        Assert.assertEquals("Qarsaalik", road.get("navn").get(0).get("navn").textValue());
-        Assert.assertNull(road.get("registreringFra"));
+        ObjectNode road1 = (ObjectNode) responseContent.get("results").get(0);
+        Assert.assertEquals("e1274f15-9e2b-4b6e-8b7d-c8078df65aa2", road1.get("uuid").textValue());
+        Assert.assertEquals("{961FFC61-8B04-45F3-80FD-509B0676FEF6}", road1.get("sumiffiik").textValue());
+        Assert.assertEquals("Qarsaalik", road1.get("navn").get(0).get("navn").textValue());
+        Assert.assertNull(road1.get("registreringFra"));
+
+        response = this.lookup("/geo/road/1/rest/search?fmt=dataonly&navn=Qarsaalik");
+        responseContent = (ObjectNode) objectMapper.readTree(response.getBody());
+        ObjectNode road2 = (ObjectNode) responseContent.get("results").get(0);
+        Assert.assertEquals(road1, road2);
+
+        response = this.lookup("/geo/road/1/rest/search?fmt=dataonly&addresseringsNavn=Qarsaalik");
+        responseContent = (ObjectNode) objectMapper.readTree(response.getBody());
+        ObjectNode road3 = (ObjectNode) responseContent.get("results").get(0);
+        Assert.assertEquals(road2, road3);
+
+        response = this.lookup("/geo/road/1/rest/search?fmt=dataonly&kommunekode=956");
+        responseContent = (ObjectNode) objectMapper.readTree(response.getBody());
+        ObjectNode road4 = (ObjectNode) responseContent.get("results").get(0);
+        Assert.assertEquals(road3, road4);
+
+        response = this.lookup("/geo/road/1/rest/search?fmt=dataonly&lokalitetskode=0600");
+        responseContent = (ObjectNode) objectMapper.readTree(response.getBody());
+        ObjectNode road5 = (ObjectNode) responseContent.get("results").get(0);
+        Assert.assertEquals(road4, road5);
+
     }
 
     @Test
@@ -82,19 +126,25 @@ public class TestGenericServices extends GeoTest {
         ResponseEntity<String> response = this.lookup("/geo/accessaddress/1/rest/search?fmt=dataonly&bnr=B-3197");
         Assert.assertEquals(200, response.getStatusCode().value());
         ObjectNode responseContent = (ObjectNode) objectMapper.readTree(response.getBody());
-        ObjectNode road = (ObjectNode) responseContent.get("results").get(0);
-        Assert.assertEquals("2e3776bf-05c2-433c-adb9-8a07df6b3e8f", road.get("uuid").textValue());
-        Assert.assertEquals("{69231C66-F37A-4F78-80C1-E379BFEE165D}", road.get("sumiffiik").textValue());
-        Assert.assertEquals("House of Testing!", road.get("blokNavn").get(0).textValue());
-        Assert.assertNull(road.get("registreringFra"));
+        ObjectNode accessAdress1 = (ObjectNode) responseContent.get("results").get(0);
+        Assert.assertEquals("2e3776bf-05c2-433c-adb9-8a07df6b3e8f", accessAdress1.get("uuid").textValue());
+        Assert.assertEquals("{69231C66-F37A-4F78-80C1-E379BFEE165D}", accessAdress1.get("sumiffiik").textValue());
+        Assert.assertEquals("House of Testing!", accessAdress1.get("blokNavn").get(0).textValue());
+        Assert.assertNull(accessAdress1.get("registreringFra"));
 
         response = this.lookup("/geo/accessaddress/1/rest/search?fmt=dataonly&vej=0254");
-        Assert.assertEquals(200, response.getStatusCode().value());
         responseContent = (ObjectNode) objectMapper.readTree(response.getBody());
-        road = (ObjectNode) responseContent.get("results").get(0);
-        Assert.assertEquals("2e3776bf-05c2-433c-adb9-8a07df6b3e8f", road.get("uuid").textValue());
-        Assert.assertEquals("{69231C66-F37A-4F78-80C1-E379BFEE165D}", road.get("sumiffiik").textValue());
-        Assert.assertEquals("House of Testing!", road.get("blokNavn").get(0).textValue());
-        Assert.assertNull(road.get("registreringFra"));
+        ObjectNode accessAdress2 = (ObjectNode) responseContent.get("results").get(0);
+        Assert.assertEquals(accessAdress1, accessAdress2);
+
+        response = this.lookup("/geo/accessaddress/1/rest/search?fmt=dataonly&husNummer=18");
+        responseContent = (ObjectNode) objectMapper.readTree(response.getBody());
+        ObjectNode accessAdress3 = (ObjectNode) responseContent.get("results").get(0);
+        Assert.assertEquals(accessAdress2, accessAdress3);
+
+        response = this.lookup("/geo/accessaddress/1/rest/search?fmt=dataonly&kommuneKode=956");
+        responseContent = (ObjectNode) objectMapper.readTree(response.getBody());
+        ObjectNode accessAdress4 = (ObjectNode) responseContent.get("results").get(0);
+        Assert.assertEquals(accessAdress3, accessAdress4);
     }
 }
