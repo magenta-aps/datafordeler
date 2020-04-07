5f7aa2424bb33726cbc5b826c3b483f7965821a1
Lars Peter Thomsen <larsp@magenta.dk>
2020-04-07 07:54:56 +0200

    Use old-style querydefinition for statistics

--- core/src/main/java/dk/magenta/datafordeler/core/database/QueryManager.java
+++ core/src/main/java/dk/magenta/datafordeler/core/database/QueryManager.java
@@ -350,7 +350,7 @@ public class QueryManager {
      */
     public static <E extends IdentifiedEntity> Stream<E> getAllEntitiesAsStream(Session session, BaseQuery query, Class<E> eClass) {
         log.debug("Get all Entities of class " + eClass.getCanonicalName() + " matching parameters " + query.getSearchParameters() + " [offset: " + query.getOffset() + ", limit: " + query.getCount() + "]");
-        org.hibernate.query.Query databaseQuery = QueryManager.getQuery(session, query);
+        org.hibernate.query.Query databaseQuery = QueryManager.getQuery(session, query, eClass);
         databaseQuery.setFlushMode(FlushModeType.COMMIT);
         databaseQuery.setFetchSize(1000);
         List<String> classNames = query.getEntityClassnames();
