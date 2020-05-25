5bda2c761f10f11cbf93e94deb916a6480482d4e
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2020-05-25 14:00:45 +0200

    removed exit

--- ci/build_plugins.sh
+++ ci/build_plugins.sh
@@ -2,6 +2,6 @@
 for plugin in "cpr cvr ger geo gladdreg adresseservice eboks prisme statistik"
 do
   cd $plugin
-  mvn --batch-mode -T 2C -DskipTests clean install || exit 1
+  mvn --batch-mode -T 2C -DskipTests clean install
   cd ..
 done
