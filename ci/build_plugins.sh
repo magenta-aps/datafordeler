30b7321ef21397580d9dc9d27b29cdf979547f6e
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2020-05-25 14:20:17 +0200

    fixed script

--- ci/build_plugins.sh
+++ ci/build_plugins.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
-for plugin in "cpr cvr ger geo gladdreg adresseservice eboks prisme statistik"
+for plugin in "cpr" "cvr" "ger" "geo" "gladdreg" "adresseservice" "eboks" "prisme" "statistik";
 do
   cd $plugin
-  mvn --batch-mode -T 2C -DskipTests clean install
+  mvn --batch-mode -T 2C -DskipTests clean install || exit 1
   cd ..
 done
