0fba08366815bafe7aa01d4f53ca494c61b2e2a3
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2020-05-25 14:26:13 +0200

    fixed typo

--- ci/build_plugins.sh
+++ ci/build_plugins.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for plugin in "cpr" "cvr" "ger" "geo" "gladdreg" "adresseservice" "eboks" "prisme" "statistik";
+for plugin in "cpr" "cvr" "ger" "geo" "gladdrreg" "adresseservice" "eboks" "prisme" "statistik";
 do
   cd $plugin
   mvn --batch-mode -T 2C -DskipTests clean install || exit 1
