94ea2fdf18594f548c0a9b3dca9e402507df8ca5
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2020-05-25 13:42:33 +0200

    fixed build_plugins.sh

--- ci/build_plugins.sh
+++ ci/build_plugins.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 for plugin in "cpr cvr ger geo gladdreg adresseservice eboks prisme statistik";
-  cd $dir
+  cd $plugin
   mvn --batch-mode -T 2C -DskipTests clean install || exit 1
   cd ..
 done
