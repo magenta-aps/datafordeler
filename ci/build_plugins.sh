2e97623e91019d213915568d071d13a59306ae9f
Mikkel Aleister Clausen <mac@magenta-aps.dk>
2020-05-25 13:53:38 +0200

    added kaniko cache

--- ci/build_plugins.sh
+++ ci/build_plugins.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 for plugin in "cpr cvr ger geo gladdreg adresseservice eboks prisme statistik"
 do
-  cd $dir
+  cd $plugin
   mvn --batch-mode -T 2C -DskipTests clean install || exit 1
   cd ..
 done
