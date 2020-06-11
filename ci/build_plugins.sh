354b5570e611f4025819520662a996359ad18033
Mads Møller Johansen <mmj@magenta.dk>
2020-06-11 11:54:37 +0200

    change build sequence

--- ci/build_plugins.sh
+++ ci/build_plugins.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-for plugin in "geo" "cpr" "cvr" "ger" "gladdrreg" "adresseservice" "eboks" "prisme" "statistik";
+for plugin in "cpr" "geo" "cvr" "ger" "gladdrreg" "adresseservice" "eboks" "prisme" "statistik";
 do
   cd $plugin
   mvn --batch-mode -T 2C -DskipTests clean install || exit 1
