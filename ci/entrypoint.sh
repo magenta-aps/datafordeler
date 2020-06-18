91a48ed4a50720bdba6c5d3288b3a81dcce7c397
Mads Møller Johansen <mmj@magenta.dk>
2020-06-18 17:54:48 +0200

    CI: splittingup test and build

---
+++ ci/entrypoint.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+#WORKDIR /app/core
+#RUN mvn test
+	
+cd /app/plugin/cpr
+mvn test
+
+cd /app/plugin/geo
+mvn test
+
+cd /app/plugin/cpr
+mvn test
+
+cd /app/plugin/geo
+mvn test
+
+cd /app/plugin/cvr
+mvn test
+
+cd /app/plugin/ger
+mvn test
+
+cd /app/plugin/eboks
+mvn test
+
+cd /app/plugin/prisme
+mvn test
+
+cd /app/plugin/statistik
+#TODO: We need to solve that tests can only pass in DK-timezone
+mvn test -Duser.timezone=Europe/Copenhagen
