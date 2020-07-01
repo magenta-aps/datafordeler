82ef8f5ac920edd76d98992b6fc4bea9f9d7d8f6
Lars Peter Thomsen <larsp@magenta.dk>
2020-07-01 17:09:13 +0200

    [#36995] Remove obsolete getLookupDefinition methods

--- plugin/statistik/src/main/java/dk/magenta/datafordeler/statistik/queries/PersonAdoptionQuery.java
+++ plugin/statistik/src/main/java/dk/magenta/datafordeler/statistik/queries/PersonAdoptionQuery.java
@@ -1,13 +1,8 @@
 package dk.magenta.datafordeler.statistik.queries;
 
-import dk.magenta.datafordeler.core.database.BaseLookupDefinition;
-import dk.magenta.datafordeler.core.database.FieldDefinition;
-import dk.magenta.datafordeler.core.database.LookupDefinition;
 import dk.magenta.datafordeler.core.exception.QueryBuildException;
 import dk.magenta.datafordeler.core.fapi.MultiCondition;
-import dk.magenta.datafordeler.core.fapi.SingleCondition;
 import dk.magenta.datafordeler.cpr.data.person.PersonEntity;
-import dk.magenta.datafordeler.cpr.records.person.data.ParentDataRecord;
 import dk.magenta.datafordeler.statistik.utils.Filter;
 
 import javax.servlet.http.HttpServletRequest;
@@ -23,20 +18,6 @@ public class PersonAdoptionQuery extends PersonStatisticsQuery {
     public PersonAdoptionQuery(Filter filter) {
         super(filter);
     }
-
-    /*@Override
-    public BaseLookupDefinition getLookupDefinition() {
-        BaseLookupDefinition lookupDefinition = super.getLookupDefinition();
-        lookupDefinition.setMatchNulls(true);
-
-        FieldDefinition fatherDefinition = this.fromPath(LookupDefinition.entityref + LookupDefinition.separator + PersonEntity.DB_FIELD_FATHER);
-        FieldDefinition motherDefinition = this.fromPath(LookupDefinition.entityref + LookupDefinition.separator + PersonEntity.DB_FIELD_MOTHER);
-
-        fatherDefinition.or(motherDefinition);
-        lookupDefinition.put(fatherDefinition);
-
-        return lookupDefinition;
-    }*/
 
     private static HashMap<String, String> joinHandles = new HashMap<>();
 
@@ -59,8 +40,5 @@ public class PersonAdoptionQuery extends PersonStatisticsQuery {
         this.applyBitemporalConditions(parentsCondition, "mother");
         this.addCondition(parentsCondition);
     }
-
-    //SELECT DISTINCT e from dk.magenta.datafordeler.cpr.data.person.PersonEntity e  LEFT JOIN e.name e_name LEFT JOIN e.name e_name LEFT JOIN e.father e_father LEFT JOIN e.mother e_mother WHERE e.identification.uuid IS NOT null  AND  ((e_father.registrationFrom >= :e_father_registrationFrom_5 OR e_mother.registrationFrom >= :e_mother_registrationFrom_6))
-
 
 }
--- plugin/statistik/src/main/java/dk/magenta/datafordeler/statistik/queries/PersonStatusQuery.java
+++ plugin/statistik/src/main/java/dk/magenta/datafordeler/statistik/queries/PersonStatusQuery.java
@@ -1,24 +1,18 @@
 package dk.magenta.datafordeler.statistik.queries;
 
-import dk.magenta.datafordeler.core.database.BaseLookupDefinition;
-import dk.magenta.datafordeler.core.database.FieldDefinition;
 import dk.magenta.datafordeler.core.database.LookupDefinition;
 import dk.magenta.datafordeler.core.exception.QueryBuildException;
 import dk.magenta.datafordeler.core.fapi.Condition;
 import dk.magenta.datafordeler.core.fapi.SingleCondition;
 import dk.magenta.datafordeler.cpr.data.person.PersonEntity;
 import dk.magenta.datafordeler.cpr.records.person.data.AddressDataRecord;
-import dk.magenta.datafordeler.cpr.records.person.data.PersonStatusDataRecord;
 import dk.magenta.datafordeler.statistik.utils.Filter;
 
 import javax.servlet.http.HttpServletRequest;
-import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
 public class PersonStatusQuery extends PersonStatisticsQuery {
-
 
     public PersonStatusQuery(HttpServletRequest request) {
         super(request);
@@ -26,43 +20,6 @@ public class PersonStatusQuery extends PersonStatisticsQuery {
 
     public PersonStatusQuery(Filter filter) {
         super(filter);
-    }
-
-    @Override
-    public BaseLookupDefinition getLookupDefinition() {
-        BaseLookupDefinition lookupDefinition = super.getLookupDefinition();
-        lookupDefinition.setMatchNulls(true);
-
-        ArrayList<FieldDefinition> fieldDefinitions = new ArrayList<>();
-        FieldDefinition addressDefinition = new FieldDefinition(
-                PersonEntity.DB_FIELD_ADDRESS + LookupDefinition.separator + AddressDataRecord.DB_FIELD_MUNICIPALITY_CODE,
-                900,
-                Integer.class,
-                LookupDefinition.Operator.GTE
-        );
-        addressDefinition.and(PersonEntity.DB_FIELD_ADDRESS + LookupDefinition.separator + AddressDataRecord.DB_FIELD_UNDONE,
-                false,
-                Boolean.class,
-                LookupDefinition.Operator.EQ);
-
-        fieldDefinitions.add(addressDefinition);
-
-/*
-        FieldDefinition statusDefinition = new FieldDefinition(
-                PersonEntity.DB_FIELD_STATUS + LookupDefinition.separator + PersonStatusDataRecord.DB_FIELD_STATUS,
-                Arrays.asList(50, 60, 80, 90),
-                Integer.class,
-                LookupDefinition.Operator.NE
-        );
-        fieldDefinitions.add(statusDefinition);
-*/
-        for (FieldDefinition fieldDefinition : fieldDefinitions) {
-            applyRegistrationTimes(fieldDefinition);
-            applyEffectTimes(fieldDefinition);
-            lookupDefinition.put(fieldDefinition);
-        }
-
-        return lookupDefinition;
     }
 
     private static HashMap<String, String> joinHandles = new HashMap<>();
