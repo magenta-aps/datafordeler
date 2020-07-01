788ff1a1ea833f19a4fb4851447d759300b74f0e
Lars Peter Thomsen <larsp@magenta.dk>
2020-07-01 19:04:24 +0200

    [#36995] Cleanup statistics

--- plugin/statistik/src/main/java/dk/magenta/datafordeler/statistik/queries/PersonStatisticsQuery.java
+++ plugin/statistik/src/main/java/dk/magenta/datafordeler/statistik/queries/PersonStatisticsQuery.java
@@ -1,23 +1,19 @@
 package dk.magenta.datafordeler.statistik.queries;
 
-import dk.magenta.datafordeler.core.database.BaseLookupDefinition;
-import dk.magenta.datafordeler.core.database.FieldDefinition;
 import dk.magenta.datafordeler.core.database.LookupDefinition;
 import dk.magenta.datafordeler.core.exception.QueryBuildException;
 import dk.magenta.datafordeler.core.fapi.Condition;
 import dk.magenta.datafordeler.core.fapi.MultiCondition;
 import dk.magenta.datafordeler.core.fapi.Query;
-import dk.magenta.datafordeler.core.fapi.SingleCondition;
-import dk.magenta.datafordeler.core.util.OffsetDateTimeAdapter;
-import dk.magenta.datafordeler.cpr.data.person.PersonEntity;
 import dk.magenta.datafordeler.cpr.data.person.PersonRecordQuery;
 import dk.magenta.datafordeler.cpr.records.CprBitemporalRecord;
-import dk.magenta.datafordeler.cpr.records.person.data.ParentDataRecord;
 import dk.magenta.datafordeler.statistik.services.StatisticsService;
 import dk.magenta.datafordeler.statistik.utils.Filter;
 
 import javax.servlet.http.HttpServletRequest;
-import java.time.*;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
 import java.util.HashMap;
@@ -149,11 +145,6 @@ public class PersonStatisticsQuery extends PersonRecordQuery {
         this.effectTimeBefore = effectTimeBefore;
     }
 
-    public static String cutPath(String path) {
-        int firstSepIndex = path.indexOf(LookupDefinition.separator);
-        return (firstSepIndex == -1) ? path : path.substring(0, firstSepIndex);
-    }
-
     protected Condition getOrigintimesCondition(MultiCondition parent, String handle) throws QueryBuildException {
         MultiCondition condition = new MultiCondition(parent, "AND");
         if (this.originTimeAfter != null) {
@@ -183,81 +174,6 @@ public class PersonStatisticsQuery extends PersonRecordQuery {
         return condition;
     }
 
-
-    protected void applyOriginTimes(FieldDefinition fieldDefinition) {
-        // Omtænk denne
-        String path = cutPath(fieldDefinition.path);
-        String originTimePath = path + LookupDefinition.separator + CprBitemporalRecord.DB_FIELD_ORIGIN_DATE;
-        if (this.getOriginTimeAfter() != null) {
-            fieldDefinition.and(
-                    originTimePath,
-                    this.getOriginTimeAfter(),
-                    LocalDate.class,
-                    LookupDefinition.Operator.GTE
-            );
-        }
-        if (this.getOriginTimeBefore() != null) {
-            FieldDefinition beforeDefinition = new FieldDefinition(
-                    originTimePath,
-                    this.getOriginTimeBefore(),
-                    LocalDate.class,
-                    LookupDefinition.Operator.LTE
-            );
-            if (this.getOriginTimeAfter() == null) {
-                beforeDefinition.or(new FieldDefinition(
-                        originTimePath,
-                        null,
-                        LocalDate.class
-                ));
-            }
-            fieldDefinition.and(beforeDefinition);
-        }
-    }
-
-    protected FieldDefinition applyOriginTimes(String basePath) {
-        // Omtænk denne
-        String originTimePath = basePath + LookupDefinition.separator + CprBitemporalRecord.DB_FIELD_ORIGIN_DATE;
-        FieldDefinition fieldDefinition = null;
-        if (this.getOriginTimeAfter() != null) {
-            FieldDefinition afterDefinition = new FieldDefinition(
-                    originTimePath,
-                    this.getOriginTimeAfter(),
-                    LocalDate.class,
-                    LookupDefinition.Operator.GTE
-            );
-            fieldDefinition = PersonStatisticsQuery.and(fieldDefinition, afterDefinition);
-        }
-        if (this.getOriginTimeBefore() != null) {
-            FieldDefinition beforeDefinition = new FieldDefinition(
-                    originTimePath,
-                    this.getOriginTimeBefore(),
-                    LocalDate.class,
-                    LookupDefinition.Operator.LTE
-            );
-            if (this.getOriginTimeAfter() == null) {
-                beforeDefinition.or(new FieldDefinition(
-                        originTimePath,
-                        null,
-                        LocalDate.class
-                ));
-            }
-            fieldDefinition = PersonStatisticsQuery.and(fieldDefinition, beforeDefinition);
-        }
-        return fieldDefinition;
-    }
-
-
-    protected void applyRegistrationTimes(FieldDefinition fieldDefinition) {
-        FieldDefinition registrationDefinition = this.applyRegistrationTimes(
-                cutPath(fieldDefinition.path)
-        );
-        if (registrationDefinition != null) {
-            fieldDefinition.and(
-                    registrationDefinition
-            );
-        }
-    }
-
     protected static HashMap<String, String> getBitemporalHandles(String handle, String path) {
         HashMap<String, String> joinHandles = new HashMap<>();
         joinHandles.put(handle + "RegistrationFrom", path + LookupDefinition.separator + CprBitemporalRecord.DB_FIELD_REGISTRATION_FROM);
@@ -284,167 +200,9 @@ public class PersonStatisticsQuery extends PersonRecordQuery {
         return this.getTimeCondition(parent, fromHandle, toHandle, this.getRegistrationTimeAfter(), this.getRegistrationTimeBefore(), this.getRegistrationAt());
     }
 
-    protected FieldDefinition applyRegistrationTimes(String basePath) {
-        String registrationFromPath = basePath + LookupDefinition.separator + CprBitemporalRecord.DB_FIELD_REGISTRATION_FROM;
-        String registrationToPath = basePath + LookupDefinition.separator + CprBitemporalRecord.DB_FIELD_REGISTRATION_TO;
-        FieldDefinition fieldDefinition = null;
-        if (this.getRegistrationTimeAfter() != null) {
-            FieldDefinition afterDefinition = new FieldDefinition(
-                    registrationFromPath,
-                    this.getRegistrationTimeAfter(),
-                    OffsetDateTime.class,
-                    LookupDefinition.Operator.GTE
-            );
-            fieldDefinition = PersonStatisticsQuery.and(fieldDefinition, afterDefinition);
-        }
-        if (this.getRegistrationTimeBefore() != null) {
-            FieldDefinition beforeDefinition = new FieldDefinition(
-                    registrationFromPath,
-                    this.getRegistrationTimeBefore(),
-                    OffsetDateTime.class,
-                    LookupDefinition.Operator.LTE
-            );
-            if (this.getRegistrationTimeAfter() == null) {
-                beforeDefinition.or(new FieldDefinition(
-                        registrationFromPath,
-                        null,
-                        OffsetDateTime.class,
-                        LookupDefinition.Operator.EQ
-                ));
-            }
-            fieldDefinition = PersonStatisticsQuery.and(fieldDefinition, beforeDefinition);
-        }
-
-
-        if (this.registrationAt != null) {
-            FieldDefinition atDefinition1 = new FieldDefinition(
-                    registrationFromPath,
-                    this.registrationAt,
-                    OffsetDateTime.class,
-                    LookupDefinition.Operator.LTE
-            );
-            atDefinition1.or(
-                    registrationFromPath,
-                    null,
-                    OffsetDateTime.class
-            );
-            fieldDefinition = PersonStatisticsQuery.and(fieldDefinition, atDefinition1);
-
-            FieldDefinition atDefinition2 = new FieldDefinition(
-                    registrationToPath,
-                    this.registrationAt,
-                    OffsetDateTime.class,
-                    LookupDefinition.Operator.GTE
-            );
-            atDefinition2.or(
-                    registrationToPath,
-                    null,
-                    OffsetDateTime.class
-            );
-            fieldDefinition = PersonStatisticsQuery.and(fieldDefinition, atDefinition2);
-
-        }
-
-
-        return fieldDefinition;
-    }
-
-
-
     protected Condition getEffecttimesCondition(MultiCondition parent, String fromHandle, String toHandle) throws QueryBuildException {
         return this.getTimeCondition(parent, fromHandle, toHandle, this.getEffectTimeAfter(), this.getEffectTimeBefore(), this.getEffectAt());
     }
-
-    protected void applyEffectTimes(FieldDefinition fieldDefinition) {
-        FieldDefinition effectDefinition = this.applyEffectTimes(
-                cutPath(fieldDefinition.path)
-        );
-        if (effectDefinition != null) {
-            fieldDefinition.and(
-                    effectDefinition
-            );
-        }
-    }
-
-    protected FieldDefinition applyEffectTimes(String basePath) {
-        FieldDefinition fieldDefinition = null;
-        String effectFromPath = basePath + LookupDefinition.separator + CprBitemporalRecord.DB_FIELD_EFFECT_FROM;
-        String effectToPath = basePath + LookupDefinition.separator + CprBitemporalRecord.DB_FIELD_EFFECT_TO;
-        if (this.getEffectTimeAfter() != null) {
-            FieldDefinition afterDefinition = new FieldDefinition(
-                    effectFromPath,
-                    this.getEffectTimeAfter(),
-                    OffsetDateTime.class,
-                    LookupDefinition.Operator.GTE
-            );
-            fieldDefinition = afterDefinition;
-        }
-        if (this.getEffectTimeBefore() != null) {
-            FieldDefinition beforeDefinition = new FieldDefinition(
-                    effectFromPath,
-                    this.getEffectTimeBefore(),
-                    OffsetDateTime.class,
-                    LookupDefinition.Operator.LTE
-            );
-            if (this.getEffectTimeAfter() == null) {
-                beforeDefinition.or(new FieldDefinition(
-                        effectFromPath,
-                        null,
-                        OffsetDateTime.class
-                ));
-            }
-            fieldDefinition = PersonStatisticsQuery.and(fieldDefinition, beforeDefinition);
-        }
-
-        if (this.getEffectAt() != null) {
-            FieldDefinition atDefinition1 = new FieldDefinition(
-                    effectFromPath,
-                    this.getEffectAt(),
-                    OffsetDateTime.class,
-                    LookupDefinition.Operator.LTE
-            );
-            atDefinition1.or(new FieldDefinition(
-                    effectFromPath,
-                    null,
-                    OffsetDateTime.class
-            ));
-            fieldDefinition = PersonStatisticsQuery.and(fieldDefinition, atDefinition1);
-
-            FieldDefinition atDefinition2 = new FieldDefinition(
-                    effectToPath,
-                    this.getEffectAt(),
-                    OffsetDateTime.class,
-                    LookupDefinition.Operator.GTE
-            );
-            atDefinition2.or(
-                    effectToPath,
-                    null,
-                    OffsetDateTime.class
-            );
-            fieldDefinition = PersonStatisticsQuery.and(fieldDefinition, atDefinition2);
-        }
-
-        return fieldDefinition;
-    }
-
-    public static FieldDefinition and(FieldDefinition left, FieldDefinition right) {
-        if (right == null) return left;
-        if (left == null) return right;
-        left.and(right);
-        return left;
-    }
-
-    protected FieldDefinition fromPath(String basePath) {
-        FieldDefinition fieldDefinition = null;
-        fieldDefinition = PersonStatisticsQuery.and(fieldDefinition, this.applyOriginTimes(basePath));
-        fieldDefinition = PersonStatisticsQuery.and(fieldDefinition, this.applyRegistrationTimes(basePath));
-        fieldDefinition = PersonStatisticsQuery.and(fieldDefinition, this.applyEffectTimes(basePath));
-        return fieldDefinition;
-    }
-
-
-
-
 
     private Condition getTimeCondition(MultiCondition parent, String fromHandle, String toHandle, OffsetDateTime afterTime, OffsetDateTime beforeTime, OffsetDateTime atTime) throws QueryBuildException {
         MultiCondition condition = new MultiCondition(parent, "AND");
