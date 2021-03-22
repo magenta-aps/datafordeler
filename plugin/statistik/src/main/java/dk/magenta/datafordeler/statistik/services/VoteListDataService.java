ed3efceecf55dc9158ae7b43af14c19dbc0b2029
Mads Møller Johansen <mmj@magenta.dk>
2021-03-22 09:37:37 -0300

    42405: Adding service for extracting vote lists

---
+++ plugin/statistik/src/main/java/dk/magenta/datafordeler/statistik/services/VoteListDataService.java
@@ -0,0 +1,257 @@
+package dk.magenta.datafordeler.statistik.services;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.dataformat.csv.CsvMapper;
+import dk.magenta.datafordeler.core.database.SessionManager;
+import dk.magenta.datafordeler.core.exception.*;
+import dk.magenta.datafordeler.core.user.DafoUserManager;
+import dk.magenta.datafordeler.cpr.CprPlugin;
+import dk.magenta.datafordeler.cpr.data.person.PersonEntity;
+import dk.magenta.datafordeler.cpr.data.person.PersonRecordQuery;
+import dk.magenta.datafordeler.cpr.records.CprBitemporalRecord;
+import dk.magenta.datafordeler.cpr.records.person.data.*;
+import dk.magenta.datafordeler.geo.GeoLookupDTO;
+import dk.magenta.datafordeler.geo.GeoLookupService;
+import dk.magenta.datafordeler.statistik.queries.PersonStatusQuery;
+import dk.magenta.datafordeler.statistik.utils.Filter;
+import org.apache.commons.lang.StringUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.time.LocalDateTime;
+import java.util.*;
+
+
+@RestController
+@RequestMapping("/statistik/vote_list_data")
+public class VoteListDataService extends PersonStatisticsService {
+
+    @Autowired
+    SessionManager sessionManager;
+
+    @Autowired
+    ObjectMapper objectMapper;
+
+    @Autowired
+    private CsvMapper csvMapper;
+
+    @Autowired
+    private DafoUserManager dafoUserManager;
+
+    @Autowired
+    private CprPlugin cprPlugin;
+
+    private Logger log = LogManager.getLogger(BirthDataService.class.getCanonicalName());
+
+    @Override
+    protected String[] requiredParameters() {
+        return new String[]{StatisticsService.EFFECT_DATE_PARAMETER};
+    }
+
+    @Override
+    protected CprPlugin getCprPlugin() {
+        return this.cprPlugin;
+    }
+
+    /**
+     * Calls handlerequest in super with the ID of the report as a parameter
+     * @param request
+     * @param response
+     * @throws AccessDeniedException
+     * @throws AccessRequiredException
+     * @throws InvalidTokenException
+     * @throws IOException
+     * @throws MissingParameterException
+     * @throws InvalidClientInputException
+     * @throws HttpNotFoundException
+     * @throws InvalidCertificateException
+     */
+    @RequestMapping(method = RequestMethod.GET, path = "/")
+    public void get(HttpServletRequest request, HttpServletResponse response)
+            throws AccessDeniedException, AccessRequiredException, InvalidTokenException, InvalidClientInputException, IOException, HttpNotFoundException, MissingParameterException, InvalidCertificateException {
+        super.handleRequest(request, response, ServiceName.STATUS);
+    }
+
+    /**
+     * Post is used for starting the generation of a report
+     * @param request
+     * @param response
+     * @throws AccessDeniedException
+     * @throws AccessRequiredException
+     * @throws InvalidTokenException
+     * @throws IOException
+     * @throws MissingParameterException
+     * @throws InvalidClientInputException
+     * @throws HttpNotFoundException
+     * @throws InvalidCertificateException
+     */
+    @RequestMapping(method = RequestMethod.POST, path = "/")
+    public void handlePost(HttpServletRequest request, HttpServletResponse response)
+            throws AccessDeniedException, AccessRequiredException, InvalidTokenException, IOException, MissingParameterException, InvalidClientInputException, HttpNotFoundException, InvalidCertificateException {
+        super.handleRequest(request, response, ServiceName.STATUS);
+    }
+
+    @Override
+    protected List<String> getColumnNames() {
+        return Arrays.asList(new String[]{
+                PNR, BIRTHDAY_YEAR, FIRST_NAME, LAST_NAME, STATUS_CODE,
+                BIRTH_AUTHORITY, BIRTH_AUTHORITY_TEXT, CITIZENSHIP_CODE, MOTHER_PNR, FATHER_PNR, CIVIL_STATUS, SPOUSE_PNR,
+                MUNICIPALITY_CODE, LOCALITY_NAME, LOCALITY_CODE, LOCALITY_ABBREVIATION, ROAD_CODE, ROAD_NAME, HOUSE_NUMBER, FLOOR_NUMBER, DOOR_NUMBER,
+                BNR, MOVING_IN_DATE, MOVE_PROD_DATE, POST_CODE, CIVIL_STATUS_DATE, CIVIL_STATUS_PROD_DATE, CHURCH, PROTECTION_TYPE
+        });
+    }
+
+    @Override
+    protected SessionManager getSessionManager() {
+        return this.sessionManager;
+    }
+
+    @Override
+    protected CsvMapper getCsvMapper() {
+        return this.csvMapper;
+    }
+
+    @Override
+    protected DafoUserManager getDafoUserManager() {
+        return this.dafoUserManager;
+    }
+
+    @Override
+    protected Logger getLogger() {
+        return this.log;
+    }
+
+    @Override
+    protected PersonRecordQuery getQuery(Filter filter) {
+        return new PersonStatusQuery(filter);
+    }
+
+    @Override
+    protected List<Map<String, String>> formatPerson(PersonEntity person, Session session, GeoLookupService lookupService, Filter filter) {
+        return Collections.singletonList(this.formatPersonByRecord(person, session, lookupService, filter));
+    }
+
+    protected Map<String, String> formatPersonByRecord(PersonEntity person, Session session, GeoLookupService lookupService, Filter filter) {
+        HashMap<String, String> item = new HashMap<>();
+
+        item.put(PNR, formatPnr(person.getPersonnummer()));
+
+        // Loop over the list of registrations (which is already sorted (by time, ascending))
+        NameDataRecord nameDataRecord = filter(person.getName(), filter);
+        if(nameDataRecord!=null) {
+            String firstname = nameDataRecord.getFirstNames();
+            if(!StringUtils.isEmpty(nameDataRecord.getMiddleName())) {
+                firstname += " "+nameDataRecord.getMiddleName();
+            }
+            item.put(FIRST_NAME, firstname);
+            item.put(LAST_NAME, nameDataRecord.getLastName());
+        }
+
+        BirthPlaceDataRecord birthPlaceDataRecord = filter(person.getBirthPlace(), filter);
+        if(birthPlaceDataRecord!=null) {
+            item.put(BIRTH_AUTHORITY, Integer.toString(birthPlaceDataRecord.getAuthority()));
+            item.put(BIRTH_AUTHORITY_CODE_TEXT, birthPlaceDataRecord.getBirthPlaceName());
+            item.put(BIRTH_AUTHORITY_TEXT, birthPlaceDataRecord.getBirthPlaceName());
+        }
+
+        BirthTimeDataRecord birthTimeDataRecord = filter(person.getBirthTime(), filter);
+        if(birthTimeDataRecord!=null) {
+            LocalDateTime birthTime = birthTimeDataRecord.getBirthDatetime();
+            if (birthTime != null) {
+                item.put(BIRTHDAY_YEAR, Integer.toString(birthTime.getYear()));
+            }
+        }
+
+        PersonStatusDataRecord statusDataRecord = filter(person.getStatus(), filter);
+        if(statusDataRecord!=null) {
+            item.put(STATUS_CODE, formatStatusCode(statusDataRecord.getStatus()));
+        }
+
+        CitizenshipDataRecord citizenshipDataRecord = filter(person.getCitizenship(), filter);
+        if(citizenshipDataRecord!=null) {
+            item.put(CITIZENSHIP_CODE, Integer.toString(citizenshipDataRecord.getCountryCode()));
+        }
+
+        ParentDataRecord mparentDataRecord = filter(person.getMother(), filter);
+        if(mparentDataRecord!=null) {
+            item.put(MOTHER_PNR, formatPnr(mparentDataRecord.getCprNumber()));
+        }
+
+        ParentDataRecord fparentDataRecord = filter(person.getFather(), filter);
+        if(fparentDataRecord!=null) {
+            item.put(FATHER_PNR, formatPnr(fparentDataRecord.getCprNumber()));
+        }
+
+        CivilStatusDataRecord civilStatusDataRecord = filter(person.getCivilstatus(), filter);
+        if(civilStatusDataRecord!=null) {
+            item.put(SPOUSE_PNR, formatPnr(civilStatusDataRecord.getSpouseCpr()));
+        }
+
+        ChurchDataRecord churchDataRecord = filter(person.getChurchRelation(), filter);
+        if(churchDataRecord!=null) {
+            item.put(CHURCH, churchDataRecord.getChurchRelation().toString());
+        }
+
+        ProtectionDataRecord protectionDataRecord = filter(person.getProtection(), filter);
+        if(protectionDataRecord!=null) {
+            item.put(PROTECTION_TYPE, Integer.toString(protectionDataRecord.getProtectionType()));
+        }
+
+        CivilStatusDataRecord civilStatusDataRecordr = filter(person.getCivilstatus(), filter);
+        if(civilStatusDataRecordr!=null) {
+            item.put(CIVIL_STATUS, civilStatusDataRecordr.getCivilStatus());
+            item.put(CIVIL_STATUS_DATE, formatTime(civilStatusDataRecordr.getEffectFrom()));
+            item.put(CIVIL_STATUS_PROD_DATE, formatTime(civilStatusDataRecordr.getRegistrationFrom()));
+        }
+
+        AddressDataRecord addressDataRecord = filter(person.getAddress(), filter);
+        if(addressDataRecord!=null) {
+            if (addressDataRecord.getMunicipalityCode() < 900) return null;
+            item.put(MOVING_IN_DATE, formatTime(addressDataRecord.getEffectFrom()));
+            item.put(MOVE_PROD_DATE, formatTime(addressDataRecord.getRegistrationFrom()));
+
+            item.put(MUNICIPALITY_CODE, formatMunicipalityCode(addressDataRecord.getMunicipalityCode()));
+            item.put(ROAD_CODE, formatRoadCode(addressDataRecord.getRoadCode()));
+            item.put(HOUSE_NUMBER, formatHouseNnr(addressDataRecord.getHouseNumber()));
+            item.put(DOOR_NUMBER, formatDoor(addressDataRecord.getDoor()));
+            item.put(BNR, formatBnr(addressDataRecord.getBuildingNumber()));
+            item.put(FLOOR_NUMBER, formatFloor(addressDataRecord.getFloor()));
+
+            // Use the lookup service to extract locality & postcode data from a municipality code and road code
+            GeoLookupDTO lookup = lookupService.doLookup(
+                    addressDataRecord.getMunicipalityCode(),
+                    addressDataRecord.getRoadCode(),
+                    addressDataRecord.getHouseNumber()
+            );
+            if (lookup != null) {
+                item.put(LOCALITY_NAME, lookup.getLocalityName());
+                item.put(LOCALITY_CODE, lookup.getLocalityCode());
+                item.put(LOCALITY_ABBREVIATION, lookup.getLocalityAbbrev());
+                item.put(POST_CODE, Integer.toString(lookup.getPostalCode()));
+                item.put(ROAD_NAME, lookup.getRoadName());
+            }
+        }
+
+
+        replaceMapValues(item, null, "");
+
+        return item;
+    }
+
+    private static <R extends CprBitemporalRecord> R filter(Collection<R> records, Filter filter) {
+        return findMostImportant(filterRecordsByEffect(filterRecordsByRegistration(filterUndoneRecords(records), filter.registrationAt), filter.effectAt));
+    }
+
+    private static <R extends CprBitemporalRecord> List<R> sort(Collection<R> records) {
+        List<R> sorted = sortRecords(filterUndoneRecords(records));
+        return sorted;
+    }
+}
---
+++ plugin/statistik/src/test/java/dk/magenta/datafordeler/statistik/VoteListDataServiceTest.java
@@ -0,0 +1,125 @@
+package dk.magenta.datafordeler.statistik;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import dk.magenta.datafordeler.core.Application;
+import dk.magenta.datafordeler.core.database.SessionManager;
+import dk.magenta.datafordeler.core.util.InputStreamReader;
+import dk.magenta.datafordeler.cpr.CprRolesDefinition;
+import dk.magenta.datafordeler.statistik.services.StatisticsService;
+import dk.magenta.datafordeler.statistik.services.StatusDataService;
+import dk.magenta.datafordeler.statistik.services.VoteListDataService;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = Application.class)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_CLASS)
+public class VoteListDataServiceTest extends TestBase {
+
+    @Autowired
+    private SessionManager sessionManager;
+
+    @Autowired
+    private TestRestTemplate restTemplate;
+
+    @Autowired
+    private TestUtils testsUtils;
+
+    @Autowired
+    private VoteListDataService voteListDataService;
+
+    private TestUserDetails testUserDetails;
+
+    @Autowired
+    private TestUtil testUtil;
+
+    @Before
+    public void initialize() throws Exception {
+        testsUtils.setPath();
+        testsUtils.loadPersonData("statusperson.txt");
+        this.loadAllGeoAdress(sessionManager);
+        voteListDataService.setUseTimeintervallimit(false);
+    }
+
+    @After
+    public void cleanup() {
+        testsUtils.clearPath();
+        testsUtils.deleteAll();
+    }
+
+    @Test
+    public void testStatusDataService() throws JsonProcessingException {
+        voteListDataService.setWriteToLocalFile(false);
+
+        ResponseEntity<String> response;// = restTemplate.exchange("/statistik/status_data/?effectDate=2018-04-16", HttpMethod.GET, new HttpEntity<>("", new HttpHeaders()), String.class);
+        //Assert.assertEquals(403, response.getStatusCodeValue());
+
+        testUserDetails = new TestUserDetails();
+        testUserDetails.giveAccess(CprRolesDefinition.READ_CPR_ROLE);
+        testUserDetails.giveAccess(StatistikRolesDefinition.EXECUTE_STATISTIK_ROLE);
+        testsUtils.applyAccess(testUserDetails);
+
+        response = restTemplate.exchange("/statistik/vote_list_data/?effectDate=2018-07-01&registrationAt=2018-08-01", HttpMethod.GET, new HttpEntity<>("", new HttpHeaders()), String.class);
+        Assert.assertEquals(200, response.getStatusCodeValue());
+        Assert.assertNotNull("Response contains a body", response);
+
+        String expected = "\"Pnr\";\"FoedAar\";\"Fornavn\";\"Efternavn\";\"Status\";\"FoedMynKod\";\"FoedMynTxt\";\"StatKod\";\"M_Pnr\";\"F_Pnr\";\"CivSt\";\"AegtePnr\";\"KomKod\";\"LokNavn\";\"LokKode\";\"LokKortNavn\";\"VejKod\";\"VejNavn\";\"HusNr\";\"Etage\";\"SideDoer\";\"Bnr\";\"TilFlyDto\";\"FlytProdDto\";\"Postnr\";\"CivDto\";\"CivProdDto\";\"Kirke\";\"ProtectionType\"\n" +
+                "\"0101001234\";\"2000\";\"Tester Testmember\";\"Testersen\";\"05\";\"9516\";\"\";\"5100\";\"2903641234\";\"0101641234\";\"G\";\"0202994321\";\"0956\";\"Nuuk\";\"0600\";\"NUK\";\"0254\";\"Qarsaalik\";\"0018\";\"01\";\"tv\";\"1234\";\"30-08-2016\";\"31-08-2016\";\"3900\";\"12-10-2017\";\"13-10-2017\";\"F\";\"1\"";
+        Assert.assertEquals(
+                testUtil.csvToJsonString(expected),
+                testUtil.csvToJsonString(response.getBody().trim())
+        );
+    }
+
+    @Test
+    public void testFileOutput() throws IOException {
+        voteListDataService.setWriteToLocalFile(true);
+
+        ResponseEntity<String> response = restTemplate.exchange("/statistik/vote_list_data/?effectDate=2018-05-01&registrationAt=2018-08-01", HttpMethod.GET, new HttpEntity<>("", new HttpHeaders()), String.class);
+        Assert.assertEquals(403, response.getStatusCodeValue());
+
+        testUserDetails = new TestUserDetails();
+        testUserDetails.giveAccess(CprRolesDefinition.READ_CPR_ROLE);
+        testUserDetails.giveAccess(StatistikRolesDefinition.EXECUTE_STATISTIK_ROLE);
+        testsUtils.applyAccess(testUserDetails);
+
+        response = restTemplate.exchange("/statistik/vote_list_data/?effectDate=2018-07-01&registrationAt=2018-08-01", HttpMethod.GET, new HttpEntity<>("", new HttpHeaders()), String.class);
+
+        Assert.assertEquals(200, response.getStatusCodeValue());
+
+        String[] statusFiles = new File(StatisticsService.PATH_FILE).list((dir, name) -> name.startsWith(StatisticsService.ServiceName.STATUS.getIdentifier()));
+        Assert.assertEquals(1, statusFiles.length);
+
+        FileInputStream fileInputStream = new FileInputStream(StatisticsService.PATH_FILE + File.separator + statusFiles[0]);
+        String contents = InputStreamReader.readInputStream(
+                fileInputStream,"UTF-8"
+        );
+        fileInputStream.close();
+
+        String expected = "\"Pnr\";\"FoedAar\";\"Fornavn\";\"Efternavn\";\"Status\";\"FoedMynKod\";\"FoedMynTxt\";\"StatKod\";\"M_Pnr\";\"F_Pnr\";\"CivSt\";\"AegtePnr\";\"KomKod\";\"LokNavn\";\"LokKode\";\"LokKortNavn\";\"VejKod\";\"VejNavn\";\"HusNr\";\"Etage\";\"SideDoer\";\"Bnr\";\"TilFlyDto\";\"FlytProdDto\";\"Postnr\";\"CivDto\";\"CivProdDto\";\"Kirke\";\"ProtectionType\"\n" +
+                "\"0101001234\";\"2000\";\"Tester Testmember\";\"Testersen\";\"05\";\"9516\";\"\";\"5100\";\"2903641234\";\"0101641234\";\"G\";\"0202994321\";\"0956\";\"Nuuk\";\"0600\";\"NUK\";\"0254\";\"Qarsaalik\";\"0018\";\"01\";\"tv\";\"1234\";\"30-08-2016\";\"31-08-2016\";\"3900\";\"12-10-2017\";\"13-10-2017\";\"F\";\"1\"";
+        Assert.assertEquals(
+                testUtil.csvToJsonString(expected),
+                testUtil.csvToJsonString(contents.trim())
+        );
+    }
+
+}
