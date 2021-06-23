9b78d18ab2b3bc3e9de94665bb5708d2d52bd6c5
Mads Møller Johansen <mmj@magenta.dk>
2021-06-23 12:18:57 -0200

    44045: Adding new attributes to gar

---
+++ plugin/geo/src/main/java/dk/magenta/datafordeler/geo/data/locality/LocalityStatusRecord.java
@@ -0,0 +1,59 @@
+package dk.magenta.datafordeler.geo.data.locality;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import dk.magenta.datafordeler.core.database.DatabaseEntry;
+import dk.magenta.datafordeler.geo.GeoPlugin;
+import dk.magenta.datafordeler.geo.data.common.GeoMonotemporalRecord;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Index;
+import javax.persistence.Table;
+import java.util.Objects;
+
+@Entity
+@Table(name = GeoPlugin.DEBUG_TABLE_PREFIX + LocalityStatusRecord.TABLE_NAME, indexes = {
+        @Index(
+                name = GeoPlugin.DEBUG_TABLE_PREFIX + LocalityStatusRecord.TABLE_NAME + LocalityStatusRecord.DB_FIELD_ENTITY,
+                columnList = LocalityStatusRecord.DB_FIELD_ENTITY + DatabaseEntry.REF
+        ),
+        @Index(
+                name = GeoPlugin.DEBUG_TABLE_PREFIX + LocalityStatusRecord.TABLE_NAME + LocalityStatusRecord.DB_FIELD_TYPE,
+                columnList = LocalityStatusRecord.DB_FIELD_TYPE
+        ),
+})
+public class LocalityStatusRecord extends GeoMonotemporalRecord<GeoLocalityEntity> {
+
+    public static final String TABLE_NAME = "geo_locality_status";
+
+    public LocalityStatusRecord() {
+    }
+
+    public LocalityStatusRecord(Integer status) {
+        this.status = status;
+    }
+
+
+
+    public static final String DB_FIELD_TYPE = "status";
+    public static final String IO_FIELD_TYPE = "status";
+    @Column(name = DB_FIELD_TYPE)
+    @JsonProperty(IO_FIELD_TYPE)
+    private Integer status;
+
+    public Integer getType() {
+        return this.status;
+    }
+
+    public void setType(Integer type) {
+        this.status = status;
+    }
+
+    public boolean equalData(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        if (!super.equalData(o)) return false;
+        LocalityStatusRecord that = (LocalityStatusRecord) o;
+        return Objects.equals(this.status, that.status);
+    }
+}
