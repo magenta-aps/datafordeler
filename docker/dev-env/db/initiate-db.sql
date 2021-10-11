bfd6dec5eba5169e716032409a8e4aad1c412e4f
Mads Møller Johansen <mmj@magenta.dk>
2021-10-11 10:08:12 +0200

    45680: Add posibility of adding testdata

---
+++ docker/dev-env/db/initiate-db.sql
@@ -0,0 +1,3 @@
+
+UPDATE Datafordeler.dbo.cpr_config SET personRegisterType = 1, personRegisterLocalFile = '/app/dev-env/local/cpr/download/d170608.l534902', personRegisterFtpAddress = 'ftp://dummy'
+GO
