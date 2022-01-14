30ef80c8221bb655b1cb5d052bb9d3f06ecbc74b
Mads Møller Johansen <mmj@magenta.dk>
2022-01-14 09:10:40 +0100

    46932: refactor

--- plugin/eskat/src/main/java/dk/magenta/datafordeler/eskat/output/PunitEntity.java
+++ plugin/eskat/src/main/java/dk/magenta/datafordeler/eskat/output/PunitEntity.java
@@ -1,6 +1,9 @@
 package dk.magenta.datafordeler.eskat.output;
 
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
 public class PunitEntity {
     private String pNummer;
     private String name;
@@ -13,6 +16,8 @@ public class PunitEntity {
 
     }
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("pnummer")
     public String getpNummer() {
         return pNummer;
     }
@@ -21,6 +26,8 @@ public class PunitEntity {
         this.pNummer = pNummer;
     }
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("firmaNavn")
     public String getName() {
         return name;
     }
@@ -29,6 +36,8 @@ public class PunitEntity {
         this.name = name;
     }
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    @JsonProperty("landekode")
     public String getLand() {
         return land;
     }
