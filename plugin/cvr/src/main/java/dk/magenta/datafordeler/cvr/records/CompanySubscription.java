dddc46bddbc44276d5bebf5be2226c807306102f
Mads Møller Johansen <mmj@magenta.dk>
2022-06-15 10:51:52 +0200

    50809: subscribe missing cvr

---
+++ plugin/cvr/src/main/java/dk/magenta/datafordeler/cvr/records/CompanySubscription.java
@@ -0,0 +1,28 @@
+package dk.magenta.datafordeler.cvr.records;
+
+import dk.magenta.datafordeler.core.database.DatabaseEntry;
+import dk.magenta.datafordeler.cpr.CprPlugin;
+import javax.persistence.*;
+
+@Entity
+@Table(name = CprPlugin.DEBUG_TABLE_PREFIX + "cvr_company_subscription")
+public class CompanySubscription extends DatabaseEntry {
+
+    public static final String DB_FIELD_CVR_NUMBER = "cvrNumber";
+
+    public CompanySubscription(Integer cvrNumber) {
+        this.cvrNumber = cvrNumber;
+    }
+
+
+    @Column(name = DB_FIELD_CVR_NUMBER, nullable = false, unique = true)
+    private Integer cvrNumber;
+
+    public Integer getCvrNumber() {
+        return this.cvrNumber;
+    }
+
+    public void setCvrNumber(Integer cvrNumber) {
+        this.cvrNumber = cvrNumber;
+    }
+}
