d238452a2fa7fc0fcbe1c893c45ba397a8fb9cc8
Mads Møller Johansen <mmj@magenta.dk>
2022-06-22 10:04:05 +0200

    51008: Remove unused

--- sts/sts/sts-saml/settings.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-
-set VERSION=0.0.1
-set WARNAME=dafo-sts-saml-%VERSION%-SNAPSHOT.war
--- sts/token_client/configure_environment.bat
+++
@@ -1,7 +0,0 @@
-@echo off
-
-REM set root dir
-set PROJECT_ROOT_DIR=%~dp0%
-
-REM load virtual env
-call "%PROJECT_ROOT_DIR%python-env\Scripts\activate"
