4671d1654d05e3205d4c4a71c82f5e2eb0095cfe
Lars Peter Thomsen <larsp@magenta.dk>
2023-04-05 11:15:49 +0200

    Traversal for units and participants

--- plugin/cvr/src/main/java/dk/magenta/datafordeler/cvr/records/ParticipantRecord.java
+++ plugin/cvr/src/main/java/dk/magenta/datafordeler/cvr/records/ParticipantRecord.java
@@ -671,8 +671,25 @@ public class ParticipantRecord extends CvrEntityRecord {
         return subs;
     }
 
-
+    @Override
     public ArrayList<CvrBitemporalRecord> closeRegistrations() {
-        return new ArrayList<>();
+        ArrayList<CvrBitemporalRecord> updated = new ArrayList<>();
+        updated.addAll(CvrBitemporalRecord.closeRegistrations(this.names));
+        updated.addAll(CvrBitemporalRecord.closeRegistrations(this.locationAddress));
+        updated.addAll(CvrBitemporalRecord.closeRegistrations(this.businessAddress));
+        updated.addAll(CvrBitemporalRecord.closeRegistrations(this.phoneNumber));
+        updated.addAll(CvrBitemporalRecord.closeRegistrations(this.emailAddress));
+        updated.addAll(CvrBitemporalRecord.closeRegistrations(this.faxNumber));
+        updated.addAll(CvrBitemporalRecord.closeRegistrations(this.postalAddress));
+        for (AttributeRecord attribute : this.attributes) {
+            updated.addAll(
+                    CvrBitemporalRecord.closeRegistrations(attribute.getValues())
+            );
+        }
+        for (CompanyParticipantRelationRecord participantRelation : this.companyRelation) {
+            updated.addAll(participantRelation.closeRegistrations());
+        }
+        return updated;
     }
+
 }
