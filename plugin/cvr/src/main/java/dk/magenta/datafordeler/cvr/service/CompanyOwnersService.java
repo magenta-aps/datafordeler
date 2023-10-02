1dd7d1551493afd6601f3c57a327a696828365a0
Lars Peter Thomsen <larsp@magenta.dk>
2023-10-02 14:05:19 +0200

    [#57822] Basis for ny service

---
+++ plugin/cvr/src/main/java/dk/magenta/datafordeler/cvr/service/CompanyOwnersService.java
@@ -0,0 +1,176 @@
+package dk.magenta.datafordeler.cvr.service;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ArrayNode;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import dk.magenta.datafordeler.core.database.Bitemporal;
+import dk.magenta.datafordeler.core.database.QueryManager;
+import dk.magenta.datafordeler.core.database.SessionManager;
+import dk.magenta.datafordeler.core.exception.AccessDeniedException;
+import dk.magenta.datafordeler.core.exception.DataFordelerException;
+import dk.magenta.datafordeler.core.exception.DataStreamException;
+import dk.magenta.datafordeler.core.fapi.BaseQuery;
+import dk.magenta.datafordeler.core.fapi.OutputWrapper;
+import dk.magenta.datafordeler.core.fapi.RecordOutputWrapper;
+import dk.magenta.datafordeler.core.user.DafoUserDetails;
+import dk.magenta.datafordeler.core.user.DafoUserManager;
+import dk.magenta.datafordeler.core.util.LoggerHelper;
+import dk.magenta.datafordeler.cpr.CprRolesDefinition;
+import dk.magenta.datafordeler.cvr.access.CvrRolesDefinition;
+import dk.magenta.datafordeler.cvr.output.ParticipantRecordOutputWrapper;
+import dk.magenta.datafordeler.cvr.query.CompanyRecordQuery;
+import dk.magenta.datafordeler.cvr.query.ParticipantRecordQuery;
+import dk.magenta.datafordeler.cvr.records.*;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.hibernate.Session;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletRequest;
+import java.time.OffsetDateTime;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequestMapping("/cvr/owners/")
+public class CompanyOwnersService {
+
+    @Autowired
+    private DafoUserManager dafoUserManager;
+
+    @Autowired
+    private SessionManager sessionManager;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    private final Logger log = LogManager.getLogger(CompanyOwnersService.class.getCanonicalName());
+
+    @Autowired
+    ParticipantRecordOutputWrapper participantRecordOutputWrapper;
+
+
+    @RequestMapping(
+            path = {"/{cvr}"},
+            produces = {"application/json"}
+    )
+    public String getRest(@PathVariable("cvr") String cvr, HttpServletRequest request) throws DataFordelerException {
+        DafoUserDetails user = dafoUserManager.getUserFromRequest(request);
+        LoggerHelper loggerHelper = new LoggerHelper(this.log, request, user);
+        loggerHelper.info("Incoming request for owners of cvr " + cvr);
+        this.checkAndLogAccess(loggerHelper);
+        Session session = sessionManager.getSessionFactory().openSession();
+
+        OffsetDateTime now = OffsetDateTime.now();
+        this.applyFilter(session, Bitemporal.FILTER_EFFECTFROM_BEFORE, Bitemporal.FILTERPARAM_EFFECTFROM_BEFORE, now);
+        this.applyFilter(session, Bitemporal.FILTER_EFFECTTO_AFTER, Bitemporal.FILTERPARAM_EFFECTTO_AFTER, now);
+
+        CompanyRecordQuery companyRecordQuery = new CompanyRecordQuery();
+        companyRecordQuery.setParameter(CompanyRecordQuery.CVRNUMMER, cvr);
+
+
+        companyRecordQuery.setRegistrationAt(now);
+        companyRecordQuery.setEffectAt(now);
+        companyRecordQuery.applyFilters(session);
+        companyRecordQuery.setPage(1);
+        companyRecordQuery.setPageSize(1);
+        ObjectNode root = this.objectMapper.createObjectNode();
+
+
+        List<CompanyRecord> companyRecords = QueryManager.getAllEntities(session, companyRecordQuery, CompanyRecord.class);
+        if (companyRecords != null && !companyRecords.isEmpty()) {
+            CompanyRecord companyRecord = companyRecords.get(0);
+
+            HashMap<Long, ParticipantRecord> participantMap = new HashMap<>();
+            HashSet<CompanyParticipantRelationRecord> legaleEjere = new HashSet<>();
+            HashSet<CompanyParticipantRelationRecord> reelleEjere = new HashSet<>();
+            HashSet<CompanyParticipantRelationRecord> fuldtAnsvarligeDeltagere = new HashSet<>();
+
+            companyRecord.getParticipants().currentStream().forEach(relationRecord -> {
+                for (OrganizationRecord organizationRecord : relationRecord.getOrganizations()) {
+                    for (AttributeRecord organizationAttribute : organizationRecord.getAttributes()) {
+                        // TODO: Brug info til at tjekke om det er en ejer
+                        //legaleEjere.add(relationRecord);
+                    }
+                }
+                long participantUnitNumber = relationRecord.getParticipantUnitNumber();
+                participantMap.put(participantUnitNumber, null);
+            });
+            ParticipantRecordQuery participantRecordQuery = new ParticipantRecordQuery();
+            participantRecordQuery.setParameter(
+                    ParticipantRecordQuery.UNITNUMBER,
+                    participantMap.keySet().stream().map(Object::toString).collect(Collectors.toList())
+            );
+
+            for (ParticipantRecord participantRecord : QueryManager.getAllEntities(session, participantRecordQuery, ParticipantRecord.class)) {
+                participantMap.put(participantRecord.getUnitNumber(), participantRecord);
+            }
+
+            root.set(
+                    "legaleEjere",
+                    outputParticipantList(participantMap, legaleEjere, participantRecordQuery, participantRecordOutputWrapper)
+            );
+            root.set(
+                    "reelleEjere",
+                    outputParticipantList(participantMap, reelleEjere, participantRecordQuery, participantRecordOutputWrapper)
+            );
+            root.set(
+                    "fuldtAnsvarligeDeltagere",
+                    outputParticipantList(participantMap, fuldtAnsvarligeDeltagere, participantRecordQuery, participantRecordOutputWrapper)
+            );
+        }
+
+        try {
+            return objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(root);
+        } catch (JsonProcessingException e) {
+            throw new DataStreamException(e);
+        }
+    }
+
+    private ArrayNode outputParticipantList(HashMap<Long, ParticipantRecord> participantMap, HashSet<CompanyParticipantRelationRecord> participantRelationRecords, ParticipantRecordQuery participantRecordQuery, ParticipantRecordOutputWrapper participantRecordOutputWrapper) {
+        ArrayNode output = this.objectMapper.createArrayNode();
+        for (CompanyParticipantRelationRecord relationRecord : participantRelationRecords) {
+            ParticipantRecord participantRecord = participantMap.get(relationRecord.getParticipantUnitNumber());
+            if (participantRecord == null) {
+                ObjectNode node = this.objectMapper.createObjectNode();
+                node.put(ParticipantRecord.IO_FIELD_UNIT_NUMBER, relationRecord.getParticipantUnitNumber());
+                output.add(node);
+            } else {
+                output.add(
+                        (ObjectNode) participantRecordOutputWrapper.wrapResult(
+                                participantRecord,
+                                participantRecordQuery,
+                                OutputWrapper.Mode.DATAONLY
+                        )
+                );
+            }
+        }
+        return output;
+    }
+
+    protected void checkAndLogAccess(LoggerHelper loggerHelper) throws AccessDeniedException {
+        try {
+            loggerHelper.getUser().checkHasSystemRole(CvrRolesDefinition.READ_CVR_ROLE);
+            loggerHelper.getUser().checkHasSystemRole(CprRolesDefinition.READ_CPR_ROLE);
+        } catch (AccessDeniedException e) {
+            loggerHelper.info("Access denied: " + e.getMessage());
+            throw (e);
+        }
+    }
+
+    private void applyFilter(Session session, String filterName, String parameterName, Object parameterValue) {
+        if (session.getSessionFactory().getDefinedFilterNames().contains(filterName)) {
+            session.enableFilter(filterName).setParameter(
+                    parameterName,
+                    parameterValue
+            );
+        }
+    }
+}
