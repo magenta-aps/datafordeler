620cdb0a6605ebc5258cad5e903840ca5f1fb9e6
Peter Juelsgaard <pj@magenta.dk>
2024-02-02 12:52:31 +0100

    feat: [#Changed both primary and secondary database configuration to envvars

--- core/src/main/java/dk/magenta/datafordeler/core/database/SessionManagerConfiguration.java
+++ core/src/main/java/dk/magenta/datafordeler/core/database/SessionManagerConfiguration.java
@@ -1,6 +1,7 @@
 package dk.magenta.datafordeler.core.database;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Component;
 
@@ -10,29 +11,46 @@ import org.springframework.stereotype.Component;
 @Component
 public class SessionManagerConfiguration {
 
+    /*
     @Value("${dafo.hibernate.configuration-file:/hibernate.cfg.xml}")
     private String primaryHibernateConfigurationFile;
-
+    */
+    /*
     @Value("${dafo.hibernate.secondary-configuration-file:/hibernate_config.cfg.xml}")
     private String secondaryHibernateConfigurationFile;
-
+    */
+    /*
     public String getPrimaryHibernateConfigurationFile() {
         return primaryHibernateConfigurationFile;
     }
-
+    */
+    /*
     public String getSecondaryHibernateConfigurationFile() {
         return secondaryHibernateConfigurationFile;
     }
-    
+    */
     /*
     @Bean
     public SessionManager sessionManager(SessionManagerConfiguration configuration) {
         return new SessionManager(configuration);
     }
     */
-
+    /* TRIAL
+    @Bean
+    public SessionManager sessionManager() {
+        return new SessionManager();
+    }
+    */
+    /*
     @Bean
     public ConfigurationSessionManager configurationSessionManager(SessionManagerConfiguration configuration) {
         return new ConfigurationSessionManager(configuration);
     }
+    */
+    /* TRIAL
+    @Bean
+    public ConfigurationSessionManager configurationSessionManager() {
+        return new ConfigurationSessionManager();
+    }
+    */
 }
