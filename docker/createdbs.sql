f89555251d61bf26da997d6df4a73b8f77865925
Lars Peter Thomsen <larsp@magenta.dk>
2024-02-08 16:44:27 +0100

    Refactoring

--- docker/createdbs.sql
+++ docker/createdbs.sql
@@ -2,4 +2,10 @@ IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'Datafordeler')
 BEGIN
   CREATE DATABASE Datafordeler;
 END;
+
+IF NOT EXISTS (SELECT * FROM sys.databases WHERE name = 'DatafordelerConfig')
+BEGIN
+  CREATE DATABASE DatafordelerConfig;
+END;
+
 GO
