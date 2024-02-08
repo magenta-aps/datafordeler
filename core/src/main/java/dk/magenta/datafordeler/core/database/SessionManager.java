44e9eb5f89e6e1ca5a24f46ea42b49776fc169b1
Lars Peter Thomsen <larsp@magenta.dk>
2024-02-08 15:10:21 +0100

    Refactoring

--- core/src/main/java/dk/magenta/datafordeler/core/database/SessionManager.java
+++ core/src/main/java/dk/magenta/datafordeler/core/database/SessionManager.java
@@ -1,25 +1,14 @@
 package dk.magenta.datafordeler.core.database;
 
-//import dk.magenta.datafordeler.core.DatabaseConfiguration;
 import dk.magenta.datafordeler.core.DatabaseConfiguration;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.h2.engine.Database;
 import org.hibernate.SessionFactory;
-import org.hibernate.cfg.Configuration;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.BeanDefinition;
-import org.springframework.context.annotation.ClassPathScanningCandidateComponentProvider;
-import org.springframework.core.type.filter.AnnotationTypeFilter;
-import org.springframework.core.type.filter.AssignableTypeFilter;
-import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.stereotype.Component;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
-import java.util.HashSet;
-import java.util.Set;
 
 /**
  * A bean to obtain Sessions with. This should be autowired in, and sessions obtained with
@@ -34,8 +23,6 @@ public class SessionManager {
     private DatabaseConfiguration databaseConfiguration;
 
     private static final Logger log = LogManager.getLogger(SessionManager.class.getCanonicalName());
-
-
 
     public SessionManager() {
     }
