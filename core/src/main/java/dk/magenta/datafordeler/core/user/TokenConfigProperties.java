5594f02ccd943fbc77a85c3597c4d6fe2eef222e
Lars Peter Thomsen <larsp@magenta.dk>
2024-05-22 15:39:32 +0200

    fix: [#57822] Cleanup

--- core/src/main/java/dk/magenta/datafordeler/core/user/TokenConfigProperties.java
+++ core/src/main/java/dk/magenta/datafordeler/core/user/TokenConfigProperties.java
@@ -1,46 +1,37 @@
 package dk.magenta.datafordeler.core.user;
 
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
 
-@ConfigurationProperties("dafo.tokenvalidation")
+@Configuration
 public class TokenConfigProperties {
+
+    @Value("dafo.tokenvalidation.time-skew-in-seconds")
     private int timeSkewInSeconds = 5;
 
+    @Value("dafo.tokenvalidation.max-assertion-time-in-seconds")
     private long maxAssertionTimeInSeconds = 60 * 60;
 
+    @Value("dafo.tokenvalidation.issuer-metadata-path")
     private String issuerMetadataPath = null;
 
+    @Value("dafo.tokenvalidation.audience-uri")
     private String audienceURI = "https://data.gl/";
 
     public int getTimeSkewInSeconds() {
         return timeSkewInSeconds;
     }
 
-    public void setTimeSkewInSeconds(int timeSkewInSeconds) {
-        this.timeSkewInSeconds = timeSkewInSeconds;
-    }
-
     public long getMaxAssertionTimeInSeconds() {
         return maxAssertionTimeInSeconds;
-    }
-
-    public void setMaxAssertionTimeInSeconds(long maxAssertionTimeInSeconds) {
-        this.maxAssertionTimeInSeconds = maxAssertionTimeInSeconds;
     }
 
     public String getIssuerMetadataPath() {
         return issuerMetadataPath;
     }
 
-    public void setIssuerMetadataPath(String issuerMetadataPath) {
-        this.issuerMetadataPath = issuerMetadataPath;
-    }
-
     public String getAudienceURI() {
         return audienceURI;
     }
 
-    public void setAudienceURI(String audienceURI) {
-        this.audienceURI = audienceURI;
-    }
 }
