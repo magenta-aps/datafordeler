3fce357f3b577e6f9f028eaf1fa28de85c34ae9c
Lars Peter Thomsen <larsp@magenta.dk>
2024-09-18 08:01:46 +0200

    feat: [#62027] Dafo demo

--- core/src/main/java/dk/magenta/datafordeler/core/database/SessionManager.java
+++ core/src/main/java/dk/magenta/datafordeler/core/database/SessionManager.java
@@ -33,7 +33,7 @@ public class SessionManager {
     public SessionManager() throws IOException {
         LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
         sessionFactoryBean.setDataSource(this.dataSource());
-        sessionFactoryBean.setPackagesToScan("dk.magenta.datafordeler");
+        // sessionFactoryBean.setPackagesToScan("dk.magenta.datafordeler");
         sessionFactoryBean.setHibernateProperties(this.hibernateProperties());
         sessionFactoryBean.setAnnotatedClasses(this.managedClasses().toArray(new Class[0]));
         sessionFactoryBean.afterPropertiesSet();
