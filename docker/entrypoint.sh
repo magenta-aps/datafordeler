e9aef42d88acf1af56d141753101f3713c0a2c58
Lars Peter Thomsen <larsp@magenta.dk>
2025-04-07 13:54:27 +0200

    fix: [#63628] Fix keytool path

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -9,7 +9,7 @@ for FILE in ${TRUST_CACERTS//,/ }
 do
     BASE="${FILE##*/}"
     BASE="${BASE%.*}"
-    /usr/bin/keytool -import -trustcacerts -storepass changeit -file $FILE -cacerts -noprompt -alias $BASE
+    /opt/java/openjdk/bin/keytool -import -trustcacerts -storepass changeit -file $FILE -cacerts -noprompt -alias $BASE
 done
 
 exec "$@"
