d724e38ee4944070ce80610a9e1bb9b92d3e618b
Lars Peter Thomsen <larsp@magenta-aps.dk>
2025-04-16 15:04:18 +0200

    fix: [#63628] java 23

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -1,15 +1,16 @@
-#!/bin/bash
+#!/bin/sh
 
 java -version
 
 TRUST_CACERTS=${TRUST_CACERTS:=""}
+IFS=,
 
 # Import TRUST_CACERTS to trust-store
-for FILE in ${TRUST_CACERTS//,/ }
+for FILE in $TRUST_CACERTS;
 do
-    BASE="${FILE##*/}"
-    BASE="${BASE%.*}"
-    /usr/bin/keytool -import -trustcacerts -storepass changeit -file $FILE -cacerts -noprompt -alias $BASE
+    BASE=$(basename $FILE)
+    /opt/java/openjdk/bin/keytool -delete -alias $BASE
+    /opt/java/openjdk/bin/keytool -import -trustcacerts -storepass changeit -file $FILE -cacerts -noprompt -alias $BASE
 done
 
 exec "$@"
