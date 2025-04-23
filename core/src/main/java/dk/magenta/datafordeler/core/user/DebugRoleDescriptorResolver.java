6f28192d20a7a0c54ded7e5ccb1eb43c23186f55
Lars Peter Thomsen <larsp@magenta.dk>
2025-04-23 21:22:41 +0200

    fix: [#63628] SAML Debug

--- core/src/main/java/dk/magenta/datafordeler/core/user/DebugRoleDescriptorResolver.java
+++ core/src/main/java/dk/magenta/datafordeler/core/user/DebugRoleDescriptorResolver.java
@@ -10,6 +10,7 @@ import org.opensaml.core.xml.XMLObject;
 import org.opensaml.saml.metadata.resolver.MetadataResolver;
 import org.opensaml.saml.metadata.resolver.impl.PredicateRoleDescriptorResolver;
 import org.opensaml.saml.saml2.common.IsTimeboundSAMLObjectValidPredicate;
+import org.opensaml.saml.saml2.common.SAML2Support;
 import org.opensaml.saml.saml2.metadata.EntityDescriptor;
 import org.opensaml.saml.saml2.metadata.RoleDescriptor;
 import org.slf4j.Logger;
@@ -47,7 +48,15 @@ public class DebugRoleDescriptorResolver extends PredicateRoleDescriptorResolver
                 Iterable<RoleDescriptor> candidates = this.getCandidatesByRoleAndProtocol(entityDescriptorsSource, criteria);
                 this.log.info("Resolved {} RoleDescriptor candidates via role criteria, performing predicate filtering", Iterables.size(candidates));
 
+                for (RoleDescriptor candidate : candidates) {
+                    log.info(candidate.getDOM().getTagName());
+                    log.info(candidate.getValidUntil().toString());
+                    log.info(SAML2Support.isValid(candidate)?"true":"false");
+                }
                 Objects.requireNonNull(predicate);
+                this.log.info(this.predicateFilterCandidates(Iterables.filter(candidates, IS_VALID_PREDICATE::test), criteria, false););
+
+
                 return this.predicateFilterCandidates(Iterables.filter(candidates, predicate::test), criteria, false);
             } else if (this.isResolveViaPredicatesOnly()) {
                 Iterable<RoleDescriptor> candidates = this.getAllCandidates(entityDescriptorsSource);
