496b21e318d2035a5049d207dfd8243365fc8ca8
Lars Peter Thomsen <larsp@magenta.dk>
2025-07-02 08:58:16 +0200

    feat: [#65591] Build doc

--- docs/openapi/swagger-initializer.js
+++ docs/openapi/swagger-initializer.js
@@ -1,12 +1,12 @@
 window.onload = function() {
   window.ui = SwaggerUIBundle({
     urls: [
-      { name: "generic", url: "openapi/openapi.json" },
-      { name: "combined", url: "openapi/combinedapi.json" },
-      { name: "custody", url: "openapi/cprapi.json" },
-      { name: "prisme", url: "openapi/prismeapi.json" },
-      { name: "subscription", url: "openapi/subscriptionapi.json" },
-      { name: "eskat", url: "openapi/eskatapi.json" }
+      { name: "generic", url: "openapi/generic.json" },
+      { name: "combined", url: "openapi/combined.json" },
+      { name: "custody", url: "openapi/custody.json" },
+      { name: "prisme", url: "openapi/prisme.json" },
+      { name: "subscription", url: "openapi/subscription.json" },
+      { name: "eskat", url: "openapi/eskat.json" }
     ],
     dom_id: '#swagger-ui',
     deepLinking: true,
