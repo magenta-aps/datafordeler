ace0ed38f9277052cba0206256269f9a2ed61b1d
Lars Peter Thomsen <larsp@magenta.dk>
2025-07-02 08:34:10 +0200

    feat: [#65591] Build doc

--- docs/openapi/swagger-initializer.js
+++ docs/openapi/swagger-initializer.js
@@ -1,7 +1,4 @@
 window.onload = function() {
-  //<editor-fold desc="Changeable Configuration Block">
-
-  // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
     urls: [
       { name: "generic", url: "openapi/openapi.json" },
@@ -19,8 +16,7 @@ window.onload = function() {
     plugins: [
       SwaggerUIBundle.plugins.DownloadUrl
     ],
-    layout: "StandaloneLayout"
+    layout: "StandaloneLayout",
+    supportedSubmitMethods: [],
   });
-
-  //</editor-fold>
 };
