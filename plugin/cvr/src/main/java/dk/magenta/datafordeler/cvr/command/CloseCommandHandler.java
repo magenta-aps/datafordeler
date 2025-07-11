fff14513edbd82712a988fcccc7d9bcae096e1c2
Lars Peter Thomsen <larsp@magenta.dk>
2025-07-11 15:46:06 +0200

    feat: [#55502] Run in several transactions

--- plugin/cvr/src/main/java/dk/magenta/datafordeler/cvr/command/CloseCommandHandler.java
+++ plugin/cvr/src/main/java/dk/magenta/datafordeler/cvr/command/CloseCommandHandler.java
@@ -13,6 +13,7 @@ import dk.magenta.datafordeler.core.exception.DataFordelerException;
 import dk.magenta.datafordeler.core.exception.DataStreamException;
 import dk.magenta.datafordeler.core.exception.InvalidClientInputException;
 import dk.magenta.datafordeler.core.util.FinalWrapper;
+import dk.magenta.datafordeler.cpr.data.person.PersonEntity;
 import dk.magenta.datafordeler.cvr.entitymanager.CompanyEntityManager;
 import dk.magenta.datafordeler.cvr.entitymanager.CompanyUnitEntityManager;
 import dk.magenta.datafordeler.cvr.entitymanager.CvrEntityManager;
@@ -104,25 +105,41 @@ public class CloseCommandHandler extends CommandHandler {
                     }
 
                     try (Session session = sessionManager.getSessionFactory().openSession()) {
-                        Transaction transaction = session.beginTransaction();
-                        try {
-                            final FinalWrapper<Integer> counter = new FinalWrapper<>(0);
+                            final FinalWrapper<Integer> totalCounter = new FinalWrapper<>(0);
                             switch (commandData.type) {
                                 case "company":
-                                    CompanyRecordQuery companyRecordQuery = new CompanyRecordQuery();
-                                    if (!commandData.ids.contains("all")) {
-                                        companyRecordQuery.addParameter(CompanyRecordQuery.CVRNUMMER, commandData.ids);
-                                    } else {
-                                        companyRecordQuery.setPageSize(10000000);
+
+                                    for (int batch=1; batch<1000; batch++) {
+                                        Transaction transaction = session.beginTransaction();
+                                        try {
+                                            CompanyRecordQuery companyRecordQuery = new CompanyRecordQuery();
+                                            companyRecordQuery.setPageSize(100);
+                                            companyRecordQuery.setPage(batch);
+                                            companyRecordQuery.addOrderField(companyRecordQuery.getEntityIdentifier(), CompanyRecord.DB_FIELD_CVR_NUMBER);
+                                            if (!commandData.ids.contains("all")) {
+                                                companyRecordQuery.addParameter(CompanyRecordQuery.CVRNUMMER, commandData.ids);
+                                            }
+                                            Stream<CompanyRecord> companies = QueryManager.getAllEntitiesAsStream(session, companyRecordQuery, CompanyRecord.class);
+                                            final FinalWrapper<Integer> batchCounter = new FinalWrapper<>(0);
+                                            companies.forEach(companyRecord -> {
+                                                System.out.println(totalCounter.getInner()+" "+companyRecord.getCvrNumber());
+                                                CloseCommandHandler.this.companyEntityManager.cleanupBitemporalSets(session, Collections.singleton(companyRecord));
+                                                CloseCommandHandler.this.companyEntityManager.closeAllEligibleRegistrations(session, Collections.singleton(companyRecord));
+                                                session.flush();
+                                                batchCounter.setInner(batchCounter.getInner() + 1);
+                                                totalCounter.setInner(totalCounter.getInner() + 1);
+                                            });
+                                            if (batchCounter.getInner() == 0) {
+                                                break;
+                                            }
+                                            transaction.commit();
+                                        } catch (Exception e) {
+                                            e.printStackTrace();
+                                            transaction.rollback();
+                                        }
                                     }
-                                    Stream<CompanyRecord> companies = QueryManager.getAllEntitiesAsStream(session, companyRecordQuery, CompanyRecord.class);
-                                    companies.forEach(companyRecord -> {
-                                        System.out.println(counter.getInner()+" "+companyRecord.getCvrNumber());
-                                        CloseCommandHandler.this.companyEntityManager.cleanupBitemporalSets(session, Collections.singleton(companyRecord));
-                                        CloseCommandHandler.this.companyEntityManager.closeAllEligibleRegistrations(session, Collections.singleton(companyRecord));
-                                        session.flush();
-                                        counter.setInner(counter.getInner() + 1);
-                                    });
+
+
                                     break;
                                 case "unit":
                                     CompanyUnitRecordQuery companyUnitRecordQuery = new CompanyUnitRecordQuery();
@@ -133,11 +150,11 @@ public class CloseCommandHandler extends CommandHandler {
                                     }
                                     Stream<CompanyUnitRecord> units = QueryManager.getAllEntitiesAsStream(session, companyUnitRecordQuery, CompanyUnitRecord.class);
                                     units.forEach(companyUnitRecord -> {
-                                        System.out.println(counter.getInner()+" "+companyUnitRecord.getUnitNumber());
+                                        System.out.println(totalCounter.getInner()+" "+companyUnitRecord.getUnitNumber());
                                         CloseCommandHandler.this.companyUnitEntityManager.cleanupBitemporalSets(session, Collections.singleton(companyUnitRecord));
                                         CloseCommandHandler.this.companyUnitEntityManager.closeAllEligibleRegistrations(session, Collections.singleton(companyUnitRecord));
                                         session.flush();
-                                        counter.setInner(counter.getInner() + 1);
+                                        totalCounter.setInner(totalCounter.getInner() + 1);
                                     });
                                     break;
                                 case "participant":
@@ -149,19 +166,15 @@ public class CloseCommandHandler extends CommandHandler {
                                     }
                                     Stream<ParticipantRecord> participants = QueryManager.getAllEntitiesAsStream(session, participantRecordQuery, ParticipantRecord.class);
                                     participants.forEach(participantRecord -> {
-                                        System.out.println(counter.getInner()+" "+participantRecord.getBusinessKey());
+                                        System.out.println(totalCounter.getInner()+" "+participantRecord.getBusinessKey());
                                         CloseCommandHandler.this.participantEntityManager.cleanupBitemporalSets(session, Collections.singleton(participantRecord));
                                         CloseCommandHandler.this.participantEntityManager.closeAllEligibleRegistrations(session, Collections.singleton(participantRecord));
                                         session.flush();
-                                        counter.setInner(counter.getInner() + 1);
+                                        totalCounter.setInner(totalCounter.getInner() + 1);
                                     });
                                     break;
                             }
-                            transaction.commit();
-                        } catch (Exception e) {
-                            e.printStackTrace();
-                            transaction.rollback();
-                        }
+
                         System.out.println("Command completed");
                     }
                 }
