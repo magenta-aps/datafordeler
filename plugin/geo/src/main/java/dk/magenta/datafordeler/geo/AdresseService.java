4c528898719fc335ffb8b90a141ea2dd9b50e640
Lars Peter Thomsen <larsp@magenta.dk>
2025-09-30 12:53:42 +0200

    feat: [#66724] Strict requestmapping (define method)

--- plugin/geo/src/main/java/dk/magenta/datafordeler/geo/AdresseService.java
+++ plugin/geo/src/main/java/dk/magenta/datafordeler/geo/AdresseService.java
@@ -32,6 +32,7 @@ import org.apache.logging.log4j.Logger;
 import org.hibernate.Session;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
--- plugin/geo/src/main/java/dk/magenta/datafordeler/geo/data/fullAdresses/GenericUnitAddressService.java
+++ plugin/geo/src/main/java/dk/magenta/datafordeler/geo/data/fullAdresses/GenericUnitAddressService.java
@@ -29,6 +29,7 @@ import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
