70811586b18420a6561809546ea924dc15d982a1
Lars Peter Thomsen <larsp@magenta.dk>
2025-09-30 12:58:13 +0200

    feat: [#66724] Strict requestmapping (define method)

--- plugin/subscription/src/main/java/dk/magenta/datafordeler/subscription/services/FindCprDataEvent.java
+++ plugin/subscription/src/main/java/dk/magenta/datafordeler/subscription/services/FindCprDataEvent.java
@@ -37,6 +37,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.util.MultiValueMap;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
