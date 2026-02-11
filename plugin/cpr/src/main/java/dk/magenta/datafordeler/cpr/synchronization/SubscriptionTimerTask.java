4151a8cf693b9ea2df900792b2c1c5d8d92a79fc
Lars Peter Thomsen <larsp@magenta.dk>
2026-02-11 11:15:01 +0100

    feat: [#68733] Report job runs to pushgateway

--- plugin/cpr/src/main/java/dk/magenta/datafordeler/cpr/synchronization/SubscriptionTimerTask.java
+++ plugin/cpr/src/main/java/dk/magenta/datafordeler/cpr/synchronization/SubscriptionTimerTask.java
@@ -1,6 +1,7 @@
 package dk.magenta.datafordeler.cpr.synchronization;
 
 import dk.magenta.datafordeler.core.AbstractTask;
+import dk.magenta.datafordeler.core.JobReporter;
 import dk.magenta.datafordeler.core.command.Worker;
 import dk.magenta.datafordeler.cpr.data.person.PersonEntityManager;
 import org.apache.logging.log4j.LogManager;
@@ -12,16 +13,23 @@ import java.time.*;
 public class SubscriptionTimerTask extends Worker {
     private final Logger log = LogManager.getLogger(SubscriptionTimerTask.class.getCanonicalName());
     private final PersonEntityManager personManager;
+    private final JobReporter jobReporter;
 
-    public SubscriptionTimerTask(PersonEntityManager personManager) {
+
+    public SubscriptionTimerTask(PersonEntityManager personManager, JobReporter jobReporter) {
         this.personManager = personManager;
+        this.jobReporter = jobReporter;
     }
 
     public static class Task extends AbstractTask<SubscriptionTimerTask> {
+        public static final String DATA_PERSONMANAGER = "personManager";
+        public static final String DATA_JOB_REPORTER = "jobReporter";
+
         @Override
         protected SubscriptionTimerTask createWorker(JobDataMap dataMap) {
             return new SubscriptionTimerTask(
-                    (PersonEntityManager) dataMap.get("personManager")
+                    (PersonEntityManager) dataMap.get(DATA_PERSONMANAGER),
+                    (JobReporter) dataMap.get(DATA_JOB_REPORTER)
             );
         }
     }
@@ -37,6 +45,7 @@ public class SubscriptionTimerTask extends Worker {
             );
             if (OffsetDateTime.now().plusMinutes(30).isBefore(deadline)) {
                 this.personManager.createSubscriptionFile();
+                this.jobReporter.reportJobSuccess("cpr_subscriptions_created");
             } else {
                 log.error("It is too late for subscriptions, wait until tomorrow");
             }
