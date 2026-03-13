c3f2929385f4c3c6cf6e3a0d5568930491c70e97
Lars Peter Thomsen <larsp@magenta.dk>
2026-03-13 11:20:39 +0100

    feat: [#67392] Fjern gamle datofelter

--- plugin/cvr/src/main/java/dk/magenta/datafordeler/cvr/records/CompanyMonthlyNumbersRecord.java
+++ plugin/cvr/src/main/java/dk/magenta/datafordeler/cvr/records/CompanyMonthlyNumbersRecord.java
@@ -23,6 +23,7 @@ import java.util.Objects;
         @Index(name = CvrPlugin.DEBUG_TABLE_PREFIX + CompanyMonthlyNumbersRecord.TABLE_NAME + "__month", columnList = CompanyMonthlyNumbersRecord.DB_FIELD_MONTH),
 
         @Index(name = CvrPlugin.DEBUG_TABLE_PREFIX + CompanyMonthlyNumbersRecord.TABLE_NAME + "__" + CvrBitemporalRecord.DB_FIELD_LAST_UPDATED, columnList = CvrBitemporalRecord.DB_FIELD_LAST_UPDATED),
+        @Index(name = CvrPlugin.DEBUG_TABLE_PREFIX + CompanyMonthlyNumbersRecord.TABLE_NAME + "__" + CvrBitemporalRecord.DB_FIELD_LAST_UPDATED+"_new", columnList = CvrBitemporalRecord.DB_FIELD_LAST_UPDATED+"_new"),
         @Index(name = CvrPlugin.DEBUG_TABLE_PREFIX + CompanyMonthlyNumbersRecord.TABLE_NAME + "__" + CvrRecordPeriod.DB_FIELD_VALID_FROM, columnList = CvrRecordPeriod.DB_FIELD_VALID_FROM),
         @Index(name = CvrPlugin.DEBUG_TABLE_PREFIX + CompanyMonthlyNumbersRecord.TABLE_NAME + "__" + CvrRecordPeriod.DB_FIELD_VALID_TO, columnList = CvrRecordPeriod.DB_FIELD_VALID_TO)
 })
