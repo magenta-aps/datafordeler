0ad2fde3c116df89f64d9308fd73e18c96f913fa
Lars Peter Thomsen <larsp@magenta.dk>
2026-03-13 15:33:05 +0100

    feat: [#67392] Migrér personeventdatarecord

--- plugin/cpr/src/main/java/dk/magenta/datafordeler/cpr/records/person/data/PersonEventDataRecord.java
+++ plugin/cpr/src/main/java/dk/magenta/datafordeler/cpr/records/person/data/PersonEventDataRecord.java
@@ -8,9 +8,12 @@ import dk.magenta.datafordeler.core.database.IdentifiedEntity;
 import dk.magenta.datafordeler.cpr.CprPlugin;
 import dk.magenta.datafordeler.cpr.data.CprRecordEntity;
 import dk.magenta.datafordeler.cpr.data.person.PersonEntity;
+import dk.magenta.datafordeler.cpr.records.person.CprBitemporalPersonRecord;
 import jakarta.persistence.*;
 
 import java.time.OffsetDateTime;
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
@@ -103,10 +106,13 @@ public class PersonEventDataRecord extends CprRecordEntity {
     }
 
     public void updateTimestamp() {
+        super.updateTimestamp();
         this.timestampNew = Bitemporal.fixOffsetOut(this.timestamp);
     }
 
     public static List<String> updateFields() {
-        return List.of(DB_FIELD_TIMESTAMP);
+        ArrayList<String> list = new ArrayList<>(CprRecordEntity.updateFields());
+        list.add(DB_FIELD_TIMESTAMP);
+        return list;
     }
 }
