cc8f0ea7ebcb8bf350f25ba558f5aca3b33e0cb4
Lars Peter Thomsen <larsp@magenta.dk>
2026-05-22 22:49:51 +0200

    feat: [#67392] Migration

--- plugin/cpr/src/main/java/dk/magenta/datafordeler/cpr/records/person/data/ForeignAddressEmigrationDataRecord.java
+++ plugin/cpr/src/main/java/dk/magenta/datafordeler/cpr/records/person/data/ForeignAddressEmigrationDataRecord.java
@@ -12,6 +12,7 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Index;
 import jakarta.persistence.Table;
+import jakarta.persistence.Transient;
 
 import java.time.OffsetDateTime;
 import java.util.ArrayList;
